453e134d00ffc5b325d91c79216b6f5ac847f2e7
Saqib Ansari <nextchamp.saqib@gmail.com>
2022-01-24 18:28:21 +0530

    refactor!: replace QueryField with new Autocomplete Field
    QueryField was only needed for Doctype and Fieldname which doesn't need a separate doctype

--- frappe_analytics/api/queries.py
+++ frappe_analytics/api/queries.py
@@ -4,22 +4,16 @@
 import frappe
 
 @frappe.whitelist()
-@frappe.validate_and_sanitize_search_inputs
-def get_docfield_list(doctype, txt, searchfield, start, page_len, filters):
-	or_filters = [
-		['fieldname', 'like', '%' + txt + '%'],
-		['label', 'like', '%' + txt + '%'],
-		['fieldtype', 'like', '%' + txt + '%']
-	]
+def get_doctype_and_field_options(txt):
+	if not txt or '.' not in txt:
+		filters = {'issingle': 0}
+		if txt:
+			filters.update({'name': ['like', f'%{txt}%']})
 
-	docfields = frappe.get_all(
-		doctype,
-		fields=["name as value", "label", "fieldtype"],
-		filters=filters,
-		or_filters=or_filters,
-		limit_start=start,
-		limit_page_length=page_len,
-		order_by="idx",
-		as_list=1,
-	)
-	return docfields
+		return frappe.get_all('DocType', filters, pluck='name', limit=20)
+
+	if '.' in txt:
+		doctype = txt.split('.')[0].title()
+		meta = frappe.get_meta(doctype)
+		valid_columns = meta.get_valid_columns()
+		return [f'{meta.name}.{d}' for d in valid_columns]
--- frappe_analytics/frappe_analytics/doctype/query/query.js
+++ frappe_analytics/frappe_analytics/doctype/query/query.js
@@ -2,7 +2,31 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Query', {
-	// refresh: function(frm) {
-
-	// }
+	setup(frm) {
+		frm.set_query('primary_field', 'columns', () => {
+			return {
+				query: 'frappe_analytics.api.queries.get_doctype_and_field_options'
+			};
+		});
+		frm.set_query('secondary_field', 'columns', () => {
+			return {
+				query: 'frappe_analytics.api.queries.get_doctype_and_field_options'
+			};
+		});
+		frm.set_query('first_operand', 'filters', () => {
+			return {
+				query: 'frappe_analytics.api.queries.get_doctype_and_field_options'
+			};
+		});
+		frm.set_query('link_value', 'filters', () => {
+			return {
+				query: 'frappe_analytics.api.queries.get_doctype_and_field_options'
+			};
+		});
+		frm.set_query('sort_by', () => {
+			return {
+				query: 'frappe_analytics.api.queries.get_doctype_and_field_options'
+			};
+		});
+	}
 });
