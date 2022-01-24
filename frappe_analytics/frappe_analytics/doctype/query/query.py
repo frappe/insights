435480175d8f13dc78f40ab0395a7de7e7260dcc
Saqib Ansari <nextchamp.saqib@gmail.com>
2022-01-24 14:56:38 +0530

    feat: select QueryFilter value as QueryField

--- frappe_analytics/frappe_analytics/doctype/query/query.py
+++ frappe_analytics/frappe_analytics/doctype/query/query.py
@@ -5,4 +5,6 @@
 from frappe.model.document import Document
 
 class Query(Document):
-	pass
+	def before_save(self):
+		for filter in self.filters:
+			filter.second_operand = filter.link_value if filter.value_is_a_query_field else filter.data_value
