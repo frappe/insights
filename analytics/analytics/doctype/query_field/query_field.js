550a2df50473a49e1137da2c621aa91a747886be
Saqib Ansari <nextchamp.saqib@gmail.com>
2022-01-29 19:38:33 +0530

    chore: use concise syntax of set_query

--- analytics/analytics/doctype/query_field/query_field.js
+++ analytics/analytics/doctype/query_field/query_field.js
@@ -1,12 +1,11 @@
 // Copyright (c) 2022, Frappe Technologies Pvt. Ltd. and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Query Field', {
+frappe.ui.form.on("Query Field", {
 	setup(frm) {
-		frm.set_query("field", () => {
-			return {
-				query: "analytics.api.queries.get_doctype_and_field_options",
-			};
-		});
-	}
+		frm.set_query(
+			"field",
+			"analytics.api.queries.get_doctype_and_field_options"
+		);
+	},
 });
