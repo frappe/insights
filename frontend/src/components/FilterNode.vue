3563dbfba64ccbe4eb5367aef04b3f604983708e
Saqib Ansari <nextchamp.saqib@gmail.com>
2022-03-24 18:53:27 +0530

    feat(ui): improve filter node design

--- frontend/src/components/FilterNode.vue
+++ frontend/src/components/FilterNode.vue
@@ -1,11 +1,11 @@
 <template>
 	<div class="text-base text-slate-800">
-		<div class="flex items-center pb-1 text-sm font-medium">
-			<span v-if="level != 1" class="pr-2 font-medium text-slate-300">
-				&#8226;
-			</span>
+		<div
+			class="group flex items-baseline py-1 text-sm font-medium"
+			:class="{ 'pl-1': level != 1 }"
+		>
 			<span
-				class="mr-1 flex cursor-pointer items-center rounded border border-slate-200 px-2 py-1"
+				class="mr-1 flex cursor-pointer items-center border-slate-200 group-hover:underline"
 				@click="$emit('toggle_group_operator', { level })"
 			>
 				{{ group_operator }}
@@ -14,9 +14,13 @@
 				of the following are true
 			</span>
 		</div>
-		<div v-if="conditions && conditions.length" class="flex flex-col">
-			<div v-for="(condition, idx) in conditions" :key="idx" class="pl-4">
-				<div v-if="condition.group_operator" class="flex items-center pb-1">
+		<ul v-if="conditions && conditions.length" class="flex flex-col pl-4">
+			<li
+				v-for="(condition, idx) in conditions"
+				:key="idx"
+				class="list-disc marker:text-gray-400"
+			>
+				<div v-if="condition.group_operator" class="flex items-center">
 					<FilterNode
 						:filters="condition"
 						@toggle_group_operator="
@@ -28,59 +32,60 @@
 				</div>
 				<div
 					v-else
-					class="menu-item flex w-fit cursor-pointer items-center pb-1"
+					class="group menu-item flex w-fit cursor-pointer items-center"
 					@click="menu_open_for = idx"
 				>
-					<div class="pr-2 font-medium text-slate-300">&#8226;</div>
-					<div
-						class="flex h-10 items-center rounded border border-slate-200 px-3"
-					>
+					<div class="flex h-8 items-center rounded border-slate-200 px-1">
 						<div class="pr-1 font-medium">{{ condition.left }}</div>
 						<div class="pr-1 text-xs font-light">{{ condition.operator }}</div>
 						<div class="font-medium text-green-600">{{ condition.right }}</div>
-						<div class="flex items-center text-sm">
-							<div class="relative cursor-pointer rounded py-1 pl-4">
-								<MenuIcon />
-								<!-- Hidden Menu -->
-								<transition
-									enter-active-class="transition ease-out duration-100"
-									enter-from-class="transform opacity-0 scale-95"
-									enter-to-class="transform opacity-100 scale-100"
-									leave-active-class="transition ease-in duration-100"
-									leave-from-class="transform opacity-100 scale-100"
-									leave-to-class="transform opacity-0 scale-95"
+						<div
+							class="relative flex cursor-pointer items-center rounded py-1 pl-4 text-sm"
+						>
+							<MenuIcon
+								class="group-hover:visible"
+								:class="{
+									visible: menu_open_for == idx,
+									invisible: menu_open_for !== idx,
+								}"
+							/>
+							<!-- Hidden Menu -->
+							<transition
+								enter-active-class="transition ease-out duration-100"
+								enter-from-class="transform opacity-0 scale-95"
+								enter-to-class="transform opacity-100 scale-100"
+								leave-active-class="transition ease-in duration-100"
+								leave-from-class="transform opacity-100 scale-100"
+								leave-to-class="transform opacity-0 scale-95"
+							>
+								<div
+									v-if="menu_open_for == idx"
+									class="absolute left-4 top-6 z-10 origin-top-left rounded bg-white shadow-md ring-1 ring-slate-200"
 								>
 									<div
-										v-if="menu_open_for == idx"
-										class="absolute left-4 top-6 z-10 origin-top-left rounded bg-white shadow-md ring-1 ring-slate-200"
+										v-for="(item, menu_item_idx) in menu_items"
+										:key="menu_item_idx"
+										class="cursor-pointer whitespace-nowrap px-3 py-1"
+										:class="
+											item.is_header
+												? 'cursor-default border-b border-slate-200 text-xs font-light text-slate-400'
+												: item.is_danger_action
+												? 'border-t border-slate-200 text-red-400 hover:bg-slate-50'
+												: item.is_chain_condition
+												? 'font-light hover:bg-slate-50'
+												: ''
+										"
+										@click.prevent.stop="on_menu_item_select(item, level, idx)"
 									>
-										<div
-											v-for="(item, menu_item_idx) in menu_items"
-											:key="menu_item_idx"
-											class="cursor-pointer whitespace-nowrap px-3 py-1"
-											:class="
-												item.is_header
-													? 'cursor-default border-b border-slate-200 text-xs font-light text-slate-400'
-													: item.is_danger_action
-													? 'border-t border-slate-200 text-red-400 hover:bg-slate-50'
-													: item.is_chain_condition
-													? 'font-light hover:bg-slate-50'
-													: ''
-											"
-											@click.prevent.stop="
-												on_menu_item_select(item, level, idx)
-											"
-										>
-											{{ item.label }}
-										</div>
+										{{ item.label }}
 									</div>
-								</transition>
-							</div>
+								</div>
+							</transition>
 						</div>
 					</div>
 				</div>
-			</div>
-		</div>
+			</li>
+		</ul>
 	</div>
 </template>
 
