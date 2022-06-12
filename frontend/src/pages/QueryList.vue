5bbdcd3c30a15b84bc0f5429a3f5de39d9752ede
Saqib Ansari <nextchamp.saqib@gmail.com>
2022-06-12 16:03:29 +0530

    refactor: QueryList with Composition API

--- frontend/src/pages/QueryList.vue
+++ frontend/src/pages/QueryList.vue
@@ -3,10 +3,10 @@
 		<header class="flex h-[3.5rem] justify-between pt-3">
 			<h1 class="text-3xl font-bold text-gray-900">Queries</h1>
 			<div>
-				<Button appearance="primary" @click="create_new_query_dialog = true"> + Add Query </Button>
+				<Button appearance="primary" @click="openDialog = true"> + Add Query </Button>
 			</div>
 		</header>
-		<main class="flex h-[calc(100%-4rem)] flex-col space-y-4 py-4">
+		<main class="flex h-[calc(100%-4rem)] min-h-[26rem] flex-col space-y-4 py-4">
 			<div class="flex space-x-4">
 				<Input type="text" placeholder="Title" inputClass="!bg-gray-200" />
 				<Input type="text" placeholder="Data Source" inputClass="!bg-gray-200" />
@@ -23,7 +23,7 @@
 				<ul role="list" class="flex flex-1 flex-col divide-y divide-gray-200 overflow-y-scroll">
 					<li v-for="query in queries" :key="query.name">
 						<router-link
-							:to="{ name: 'Query', params: { query_id: query.name } }"
+							:to="{ name: 'Query', params: { name: query.name } }"
 							class="flex cursor-pointer items-center rounded-md py-3 px-4 hover:bg-gray-50"
 						>
 							<p class="mr-4"><Input type="checkbox" class="rounded-md border-gray-400" /></p>
@@ -41,83 +41,70 @@
 				</div>
 			</div>
 		</main>
-		<Dialog :options="{ title: 'New Query' }" v-model="create_new_query_dialog">
+		<Dialog :options="{ title: 'New Query' }" v-model="openDialog">
 			<template #body-content>
 				<div class="space-y-4">
-					<Input type="select" label="Data Source" v-model="new_query.data_source" :options="data_sources" />
-					<Input type="text" label="Title" v-model="new_query.title" />
+					<Input type="select" label="Data Source" v-model="newQuery.dataSource" :options="dataSources" />
+					<Input type="text" label="Title" v-model="newQuery.title" />
 				</div>
 			</template>
 			<template #actions>
-				<Button appearance="primary" @click="submit_query" :loading="$resources.create_query.loading"> Create </Button>
+				<Button appearance="primary" @click="submitQuery" :loading="createQuery.loading"> Create </Button>
 			</template>
 		</Dialog>
 	</div>
 </template>
 
-<script>
+<script setup>
 import moment from 'moment'
-import { Dialog, Button, Input } from 'frappe-ui'
+import { useRouter } from 'vue-router'
+import { computed, reactive, ref } from 'vue'
+import { Dialog, Button, Input, createResource } from 'frappe-ui'
 
-export default {
-	name: 'QueryList',
-	components: {
-		Button,
-		Dialog,
-		Input,
+const openDialog = ref(false)
+const newQuery = reactive({
+	dataSource: '',
+	title: '',
+})
+
+const getQueries = createResource('analytics.api.get_queries')
+const queries = computed(() => {
+	const queries = getQueries.data || []
+	queries.forEach((query) => {
+		query.modified_from_now = moment(query.modified).fromNow()
+	})
+	return queries
+})
+getQueries.fetch()
+
+const getDataSources = createResource({
+	method: 'analytics.api.get_data_sources',
+	onSuccess(res) {
+		newQuery.dataSource = res[0]['name']
 	},
-	data() {
-		return {
-			create_new_query_dialog: false,
-			new_query: {
-				title: '',
-				data_source: '',
-			},
-		}
+})
+const dataSources = computed(() => {
+	return getDataSources.data?.map((d) => d['name']) || []
+})
+getDataSources.fetch()
+
+const router = useRouter()
+const createQuery = createResource({
+	method: 'analytics.api.create_query',
+	onSuccess(name) {
+		newQuery.title = ''
+		newQuery.data_source = ''
+		openDialog.value = false
+		router.push(`/query/${name}`)
 	},
-	resources: {
-		get_queries: {
-			method: 'analytics.api.get_queries',
-			auto: true,
-		},
-		get_data_sources: {
-			method: 'analytics.api.get_data_sources',
-			auto: true,
-			onSuccess(data_sources) {
-				this.new_query.data_source = data_sources[0]['name']
-			},
-		},
-		create_query: {
-			method: 'analytics.api.create_query',
-			onSuccess(query_name) {
-				this.new_query.title = ''
-				this.new_query.data_source = ''
-				this.create_new_query_dialog = false
-				this.$router.push(`/query/${query_name}`)
-			},
-		},
-	},
-	computed: {
-		queries() {
-			const queries = this.$resources.get_queries.data || []
-			queries.forEach((query) => {
-				query.modified_from_now = moment(query.modified).fromNow()
-			})
-			return queries
-		},
-		data_sources() {
-			return this.$resources.get_data_sources.data.map((d) => d['name']) || []
-		},
-	},
-	methods: {
-		submit_query() {
-			if (this.new_query.title && this.new_query.data_source) {
-				this.$resources.create_query.submit({
-					title: this.new_query.title,
-					data_source: this.new_query.data_source,
-				})
-			}
-		},
-	},
+})
+
+const submitQuery = () => {
+	if (newQuery.title && newQuery.dataSource) {
+		createQuery.submit({
+			title: newQuery.title,
+			data_source: newQuery.dataSource,
+		})
+	}
 }
 </script>
