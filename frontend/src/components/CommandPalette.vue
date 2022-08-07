48944fbe9132d3132c5141daf6f481eb53fe4e31
Saqib Ansari <nextchamp.saqib@gmail.com>
2022-08-07 12:24:30 +0530

    style: fade command palette

--- frontend/src/components/CommandPalette.vue
+++ frontend/src/components/CommandPalette.vue
@@ -1,49 +1,51 @@
 <template>
-	<div
-		v-show="show"
-		class="absolute z-10 flex h-full w-full justify-center pt-20 text-base backdrop-blur-sm"
-	>
-		<div class="max-h-[28rem] w-[38rem] rounded-md border bg-white shadow-md">
-			<div class="flex items-center border-b px-3">
-				<FeatherIcon name="search" class="absolute h-4 w-4 text-gray-500" />
-				<input
-					ref="searchInput"
-					v-model="searchTerm"
-					class="ml-2 flex h-12 w-full items-center rounded-t-md px-4 focus:outline-none"
-					placeholder="Search..."
-				/>
-			</div>
-			<div class="mt-2 flex flex-col px-2">
-				<div v-if="commands.length > 0">
-					<div class="text-sm text-gray-500">Navigation</div>
-					<div
-						v-for="(command, idx) in commands"
-						class="-mx-2 flex h-10 cursor-pointer items-center space-x-2 px-2"
-						@mouseenter="activeIndex = idx"
-						:class="[activeIndex === idx ? 'bg-gray-50' : '']"
-						@click="
-							() => {
-								commandPalette.close()
-								command.action()
-							}
-						"
-					>
-						<FeatherIcon
-							:name="command.icon || 'arrow-right'"
-							class="h-4 w-4 text-gray-500"
-						/>
-						<div class="flex items-baseline">
-							<span>{{ command.title }}</span>
-							<span class="ml-2 text-sm text-gray-500">
-								{{ command.description }}
-							</span>
+	<transition name="fade">
+		<div
+			v-show="show"
+			class="absolute z-10 flex h-full w-full justify-center bg-gray-50/60 pt-20 text-base backdrop-blur-sm backdrop-grayscale"
+		>
+			<div class="max-h-[28rem] w-[38rem] rounded-md border bg-white shadow-md">
+				<div class="flex items-center border-b px-3">
+					<FeatherIcon name="search" class="absolute h-4 w-4 text-gray-500" />
+					<input
+						ref="searchInput"
+						v-model="searchTerm"
+						class="ml-2 flex h-12 w-full items-center rounded-t-md px-4 focus:outline-none"
+						placeholder="Search..."
+					/>
+				</div>
+				<div class="mt-2 flex flex-col px-2">
+					<div v-if="commands.length > 0">
+						<div class="text-sm text-gray-500">Navigation</div>
+						<div
+							v-for="(command, idx) in commands"
+							class="-mx-2 flex h-10 cursor-pointer items-center space-x-2 px-2"
+							@mouseenter="activeIndex = idx"
+							:class="[activeIndex === idx ? 'bg-gray-50' : '']"
+							@click="
+								() => {
+									commandPalette.close()
+									command.action()
+								}
+							"
+						>
+							<FeatherIcon
+								:name="command.icon || 'arrow-right'"
+								class="h-4 w-4 text-gray-500"
+							/>
+							<div class="flex items-baseline">
+								<span>{{ command.title }}</span>
+								<span class="ml-2 text-sm text-gray-500">
+									{{ command.description }}
+								</span>
+							</div>
 						</div>
 					</div>
+					<div v-else class="text-sm text-gray-500">No results found</div>
 				</div>
-				<div v-else class="text-sm text-gray-500">No results found</div>
 			</div>
 		</div>
-	</div>
+	</transition>
 </template>
 
 <script setup>
@@ -106,3 +108,15 @@ watch(enter, (pressed) => {
 	}
 })
 </script>
+
+<style>
+.fade-enter-active,
+.fade-leave-active {
+	transition: opacity 0.2s ease-out;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+	opacity: 0;
+}
+</style>
