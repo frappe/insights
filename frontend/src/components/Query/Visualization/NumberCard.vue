6b54c858029519260d1a5e383ec283ff3fde0781
Saqib Ansari <nextchamp.saqib@gmail.com>
2022-10-08 11:54:55 +0530

    fix: number card style

--- frontend/src/components/Query/Visualization/NumberCard.vue
+++ frontend/src/components/Query/Visualization/NumberCard.vue
@@ -16,9 +16,15 @@ const formattedValue = computed(() => {
 		value = value.toFixed(1)
 	}
 	const locale = 'en-IN' // TODO: get locale from user settings
-	return new Intl.NumberFormat(locale, {
+	let formatted = new Intl.NumberFormat(locale, {
 		notation: 'compact',
 	}).format(value)
+
+	if (locale == 'en-IN') {
+		formatted = formatted.replace('T', 'K')
+	}
+
+	return formatted
 })
 
 const prefix = computed(() => {
@@ -30,8 +36,8 @@ const suffix = computed(() => {
 </script>
 
 <template>
-	<div class="flex h-full min-h-[6rem] w-full min-w-40 flex-col items-center justify-center">
-		<div class="text-base font-semibold text-gray-600">{{ props.options.title }}</div>
-		<div class="text-[44px] leading-tight">{{ prefix }}{{ formattedValue }}{{ suffix }}</div>
+	<div class="flex h-full w-full min-w-40 flex-col items-center justify-center">
+		<div class="text-[38px] leading-tight">{{ prefix }}{{ formattedValue }}{{ suffix }}</div>
+		<div class="text-[14px] font-light">{{ props.options.title }}</div>
 	</div>
 </template>
