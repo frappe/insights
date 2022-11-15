04084540d815d8a61e39143137439be0f5b1e895
Saqib Ansari <nextchamp.saqib@gmail.com>
2022-11-15 13:49:47 +0530

    refactor: remove format options from column picker

--- frontend/src/components/Query/Column/ColumnEditor.vue
+++ frontend/src/components/Query/Column/ColumnEditor.vue
@@ -1,51 +1,36 @@
 <template>
 	<div class="mb-4 flex h-7 items-center">
 		<Button icon="chevron-left" class="mr-2" @click="$emit('close')"> </Button>
-		<div class="text-sm tracking-wide text-gray-600">EDIT {{ columnType.toUpperCase() }}</div>
+		<div class="text-sm uppercase tracking-wide text-gray-600">Edit {{ columnType }}</div>
 	</div>
 	<div class="flex flex-col space-y-3">
-		<div class="flex h-9 items-center space-x-2 rounded-md border bg-gray-50 p-0.5 text-sm">
-			<div
-				v-for="tab in [columnType, 'Format']"
-				class="flex h-full flex-1 items-center justify-center rounded-md"
-				:class="{
-					'border bg-white font-normal shadow-sm': currentTab == tab,
-				}"
-				@click.prevent.stop="currentTab = tab"
-			>
-				{{ tab }}
-			</div>
-		</div>
 		<MetricPicker
-			v-if="currentTab == 'Metric'"
+			v-if="columnType == 'Metric'"
 			:column="props.column"
 			@column-select="editColumn"
 			@close="$emit('close')"
 		/>
 		<DimensionPicker
-			v-if="currentTab == 'Dimension'"
+			v-if="columnType == 'Dimension'"
 			:column="props.column"
 			@column-select="editColumn"
 			@close="$emit('close')"
 		/>
 		<ColumnExpressionPicker
-			v-if="currentTab == 'Expression'"
+			v-if="columnType == 'Expression'"
 			:column="props.column"
 			@column-select="editColumn"
 			@close="$emit('close')"
 		/>
-		<ColumnFormatter v-if="currentTab == 'Format'" :column="props.column" @save="editColumn" />
 	</div>
 </template>
 
 <script setup>
 import MetricPicker from '@/components/Query/Column/MetricPicker.vue'
 import DimensionPicker from '@/components/Query/Column/DimensionPicker.vue'
-import ColumnFormatter from '@/components/Query/Column/ColumnFormatter.vue'
 import ColumnExpressionPicker from '@/components/Query/Column/ColumnExpressionPicker.vue'
 
 import { inject, ref } from 'vue'
-import { ellipsis } from '@/utils'
 
 const emit = defineEmits(['close'])
 const props = defineProps({
@@ -63,7 +48,6 @@ const columnType = ref(
 		? 'Dimension'
 		: 'Metric'
 )
-const currentTab = ref(columnType.value)
 
 const editColumn = (column) => {
 	query.updateColumn.submit({ column })
