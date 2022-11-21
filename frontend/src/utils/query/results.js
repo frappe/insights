bfa7a8cd6a03f5ae49e35f9a4d3cefc3a360e4ce
Saqib Ansari <nextchamp.saqib@gmail.com>
2022-11-21 13:32:11 +0530

    fix: format only date columns

--- frontend/src/utils/query/results.js
+++ frontend/src/utils/query/results.js
@@ -56,7 +56,7 @@ export function getFormattedResult(data, columns) {
 		const _data = unref(data)
 		const _columns = unref(columns)
 
-		if (!_data || _data.length == 0) return []
+		if (!_data || !_data.length) return []
 
 		const columnTypes = _data[0].map((c) => c.split('::')[1])
 
@@ -74,9 +74,12 @@ export function getFormattedResult(data, columns) {
 						cell = isNaN(cell) ? 0 : cell
 					}
 				}
-				const formatOption = _columns[idx]?.format_option
-				if (formatOption) {
-					cell = applyColumnFormatOption(safeJSONParse(formatOption), cell)
+				if (FIELDTYPES.DATE.includes(columnType)) {
+					// only use format options for dates
+					const formatOption = _columns[idx]?.format_option
+					if (formatOption) {
+						cell = applyColumnFormatOption(safeJSONParse(formatOption), cell)
+					}
 				}
 				return cell
 			})
