376852d35d2da5e729c195150124876ec60c979d
Saqib Ansari <nextchamp.saqib@gmail.com>
2022-11-22 12:21:41 +0530

    fix: do not pool connections

--- insights/insights/doctype/insights_data_source/sources/utils.py
+++ insights/insights/doctype/insights_data_source/sources/utils.py
@@ -2,8 +2,9 @@
 # For license information, please see license.txt
 
 import frappe
-from frappe.database.mariadb.database import MariaDBDatabase
+from sqlalchemy import create_engine
 from sqlalchemy.engine.base import Engine
+from sqlalchemy.pool import NullPool
 
 MARIADB_TO_GENERIC_TYPES = {
     "int": "Integer",
@@ -19,7 +20,6 @@ MARIADB_TO_GENERIC_TYPES = {
 
 
 def get_sqlalchemy_engine(**kwargs) -> Engine:
-    from sqlalchemy import create_engine
 
     dialect = kwargs.pop("dialect")
     driver = kwargs.pop("driver")
@@ -33,42 +33,7 @@ def get_sqlalchemy_engine(**kwargs) -> Engine:
     uri = f"{dialect}+{driver}://{user}:{password}@{host}:{port}/{database}?{extra_params}"
 
     # TODO: cache the engine by uri
-    return create_engine(uri)
-
-
-class SecureMariaDB(MariaDBDatabase):
-    def __init__(self, dbName, useSSL, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.useSSL = useSSL
-        self.dbName = dbName
-
-    def get_connection_settings(self) -> dict:
-        conn_settings = super().get_connection_settings()
-        conn_settings["ssl"] = self.useSSL
-        conn_settings["ssl_verify_cert"] = self.useSSL
-
-        if self.user != "root":
-            # Fix: cannot connect to non-frappe MariaDB instances where database name != user name
-            conn_settings["database"] = self.dbName
-        return conn_settings
-
-    def sql(self, query, *args, skip_validation=False, **kwargs):
-        self.validate_query(query, skip_validation)
-        try:
-            return super().sql(query, *args, **kwargs)
-        except Exception as e:
-            frappe.log_error(f"Error fetching data from Secure MariaDB: {e}")
-            raise
-        finally:
-            self.close()
-
-    def validate_query(self, query, skip_validation=False):
-        if skip_validation:
-            return
-        if not query.strip().lower().startswith(("select", "explain", "with", "desc")):
-            raise frappe.ValidationError(
-                "Only SELECT and EXPLAIN statements are allowed in Query Store"
-            )
+    return create_engine(uri, poolclass=NullPool)
 
 
 def create_insights_table(table, force=False):
