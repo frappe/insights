dc14d4c7073de4185e084f6b3cfc5af491f3237f
Saqib Ansari <nextchamp.saqib@gmail.com>
2022-11-25 16:39:25 +0530

    fix: need to refresh to close table selector popup

--- frontend/src/components/Query/Table/TablePanel.vue
+++ frontend/src/components/Query/Table/TablePanel.vue
@@ -9,19 +9,22 @@
 				<div class="text-sm tracking-wide text-gray-600">TABLES</div>
 				<Button icon="plus" @click="addingTable = true"></Button>
 			</div>
-			<div v-if="addingTable" class="mb-4 w-full">
-				<Autocomplete
-					ref="tableSearch"
-					v-model="newTable"
-					:options="newTableOptions"
-					placeholder="Select a table..."
-					@selectOption="
-						(table) => {
-							addingTable = false
-							table && query.addTable.submit({ table })
-						}
-					"
-				/>
+			<div v-if="addingTable" class="flex w-full space-x-2 pt-1 pb-3">
+				<div class="flex-1">
+					<Autocomplete
+						ref="tableSearch"
+						v-model="newTable"
+						:options="newTableOptions"
+						placeholder="Select a table..."
+						@selectOption="
+							(table) => {
+								addingTable = false
+								table && query.addTable.submit({ table })
+							}
+						"
+					/>
+				</div>
+				<Button icon="x" @click="addingTable = false"></Button>
 			</div>
 			<div
 				v-if="query.tables.data?.length == 0"
