1a6d5753ae9e79f1c887a1d3aae3ea6a5895dc1d
Saqib Ansari <nextchamp.saqib@gmail.com>
2022-11-25 16:45:20 +0530

    fix: filtering unique options in autocomplete

--- frontend/src/components/Controls/Autocomplete.vue
+++ frontend/src/components/Controls/Autocomplete.vue
@@ -167,7 +167,14 @@ const selectedOption = computed({
 })
 const uniqueOptions = computed(() => {
 	return options.value.filter((option, index, self) => {
-		return self.findIndex((t) => t.value === option.value && t.label === option.label) === index
+		return (
+			self.findIndex(
+				(t) =>
+					t.value === option.value &&
+					t.label === option.label &&
+					t.description === option.description
+			) === index
+		)
 	})
 })
 const filteredOptions = computed(() => {
