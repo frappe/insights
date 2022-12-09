2ad71151932da5c863634adf685b4333feabf076
Saqib Ansari <nextchamp.saqib@gmail.com>
2022-12-09 15:06:03 +0530

    fix: table chart

--- frontend/src/utils/charts/table.js
+++ frontend/src/utils/charts/table.js
@@ -15,7 +15,9 @@ function useTableChart() {
 
 	function getRows(columns, data) {
 		const columnLabels = data[0]?.map((d) => d.split('::')[0])
-		const columnIndexes = columnLabels.map((label) => columns.indexOf(label))
+		const columnIndexes = columnLabels
+			.map((label) => columns.indexOf(label))
+			.filter((i) => i >= 0)
 		return data.slice(1).map((row) => columnIndexes.map((index) => row[index]))
 	}
 
@@ -26,7 +28,9 @@ function useTableChart() {
 		const columns = queryChart.config.columns.map((c) => c.label)
 		const rows = getRows(columns, queryChart.data)
 		const title = queryChart.title
-		return { title, columns, rows, options: queryChart.config.options }
+		const props = { title, columns, rows, options: queryChart.config.options }
+		console.log(props)
+		return props
 	}
 
 	return chart
