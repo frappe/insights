a9f53a032b9faa412773dd54baa5a5e7db705b82
Saqib Ansari <nextchamp.saqib@gmail.com>
2022-12-11 20:46:02 +0530

    fix: column panel padding

--- frontend/src/components/Query/Column/ColumnPanel.vue
+++ frontend/src/components/Query/Column/ColumnPanel.vue
@@ -1,13 +1,13 @@
 <template>
 	<div class="flex h-full w-1/3 flex-col overflow-scroll px-4 pb-2">
 		<div v-if="!addingColumn && !editingColumn" class="flex h-full w-full flex-col">
-			<div class="h-10 pt-1">
+			<div class="pb-3 pt-1">
 				<div class="flex items-center justify-between bg-white">
 					<div class="text-sm tracking-wide text-gray-600">COLUMNS</div>
 					<Button icon="plus" @click="addingColumn = true"></Button>
 				</div>
 			</div>
-			<div class="h-[calc(100%-2.5rem)] w-full">
+			<div class="h-[calc(100%-3rem)] w-full">
 				<ColumnList
 					@edit-column="(column) => ([editColumn, editingColumn] = [column, true])"
 				></ColumnList>
