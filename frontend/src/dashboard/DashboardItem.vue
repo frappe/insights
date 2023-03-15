21b2cbb798cc7092cc0bebd41590d3c850eb85ba
Saqib Ansari <nextchamp.saqib@gmail.com>
2023-03-15 17:17:09 +0530

    fix: dashboard item action z-index

--- frontend/src/dashboard/DashboardItem.vue
+++ frontend/src/dashboard/DashboardItem.vue
@@ -10,7 +10,17 @@ const props = defineProps({
 	item: { type: Object, required: true },
 })
 
+function openQueryInNewTab() {
+	if (!props.item.options.query) return
+	window.open(`/insights/query/build/${props.item.options.query}`, '_blank')
+}
 const actions = [
+	{
+		icon: 'external-link',
+		label: 'Open Query',
+		hidden: (item) => item.item_type === 'Filter' || item.item_type === 'Text',
+		onClick: openQueryInNewTab,
+	},
 	{
 		icon: 'download',
 		label: 'Download',
@@ -65,72 +75,87 @@ const refreshKey = computed(() => {
 </script>
 
 <template>
-	<div class="dashboard-item h-full min-h-[60px] w-full p-1.5">
-		<div
-			class="relative flex h-full rounded-md"
-			:class="{
-				'border bg-white shadow-sm':
-					item.item_type !== 'Filter' && item.item_type !== 'Text',
-				'ring-2 ring-blue-300 ring-offset-1':
-					item.item_id === dashboard.currentItem?.item_id,
-				'cursor-grab': dashboard.editing,
-			}"
-			@click.prevent.stop="dashboard.setCurrentItem(item.item_id)"
-			@dblclick.prevent.stop="dashboard.edit() || dashboard.setCurrentItem(item.item_id)"
+	<div class="dashboard-item h-full min-h-[60px] w-full p-1.5 [&>div:first-child]:h-full">
+		<Popover
+			class="flex h-full w-full [&>div:first-child]:w-full"
+			:show="dashboard.editing && dashboard.currentItem?.item_id === item.item_id"
+			placement="top-end"
 		>
-			<div
-				v-if="chartData.loading"
-				class="absolute inset-0 z-[10000] flex h-full w-full items-center justify-center rounded-md bg-white"
-			>
-				<LoadingIndicator class="w-6 text-gray-300" />
-			</div>
+			<template #target>
+				<div
+					class="relative flex h-full rounded-md"
+					:class="{
+						'border bg-white shadow-sm':
+							item.item_type !== 'Filter' && item.item_type !== 'Text',
+						'ring-2 ring-blue-300 ring-offset-1':
+							item.item_id === dashboard.currentItem?.item_id,
+						'cursor-grab': dashboard.editing,
+					}"
+					@click.prevent.stop="dashboard.setCurrentItem(item.item_id)"
+					@dblclick.prevent.stop="
+						dashboard.edit() || dashboard.setCurrentItem(item.item_id)
+					"
+				>
+					<div
+						v-if="chartData.loading"
+						class="absolute inset-0 z-[10000] flex h-full w-full items-center justify-center rounded-md bg-white"
+					>
+						<LoadingIndicator class="w-6 text-gray-300" />
+					</div>
 
-			<component
-				ref="widget"
-				:class="[dashboard.editing ? 'pointer-events-none' : '']"
-				:is="widgets.getComponent(item.item_type)"
-				:chartData="chartData"
-				:item_id="item.item_id"
-				:options="item.options"
-				:key="refreshKey"
-			>
-				<template #placeholder>
-					<InvalidWidget
-						class="absolute"
-						title="Insufficient options"
-						icon="settings"
-						:message="null"
-						icon-class="text-gray-400"
-					/>
-				</template>
-			</component>
+					<component
+						ref="widget"
+						:class="[dashboard.editing ? 'pointer-events-none' : '']"
+						:is="widgets.getComponent(item.item_type)"
+						:chartData="chartData"
+						:item_id="item.item_id"
+						:options="item.options"
+						:key="refreshKey"
+					>
+						<template #placeholder>
+							<InvalidWidget
+								class="absolute"
+								title="Insufficient options"
+								icon="settings"
+								:message="null"
+								icon-class="text-gray-400"
+							/>
+						</template>
+					</component>
 
-			<div class="absolute top-3 right-3 z-10 flex items-center">
-				<div v-if="chartFilters?.length">
-					<Tooltip :text="chartFilters.map((c) => c.label).join(', ')">
-						<div
-							class="flex items-center space-x-1 rounded-full bg-gray-100 px-2 py-1 text-sm leading-3 text-gray-600"
-						>
-							<span>{{ chartFilters.length }}</span>
-							<FeatherIcon name="filter" class="h-3 w-3" @mousedown.prevent.stop="" />
+					<div class="absolute top-3 right-3 z-10 flex items-center">
+						<div v-if="chartFilters?.length">
+							<Tooltip :text="chartFilters.map((c) => c.label).join(', ')">
+								<div
+									class="flex items-center space-x-1 rounded-full bg-gray-100 px-2 py-1 text-sm leading-3 text-gray-600"
+								>
+									<span>{{ chartFilters.length }}</span>
+									<FeatherIcon
+										name="filter"
+										class="h-3 w-3"
+										@mousedown.prevent.stop=""
+									/>
+								</div>
+							</Tooltip>
 						</div>
-					</Tooltip>
+					</div>
 				</div>
-			</div>
+			</template>
 
-			<div
-				v-if="dashboard.editing && item.item_id === dashboard.currentItem?.item_id"
-				class="absolute -top-7 right-0 z-20 flex cursor-pointer space-x-2.5 rounded-md bg-gray-700 px-2 py-1.5 shadow-sm transition-opacity duration-200 ease-in-out"
-			>
-				<FeatherIcon
-					v-for="action in actions"
-					:key="action.label"
-					:name="action.icon"
-					class="h-3.5 w-3.5 text-white"
-					:class="{ hidden: action.hidden && action.hidden(item) }"
-					@click="action.onClick(item)"
-				/>
-			</div>
-		</div>
+			<template #body>
+				<div
+					class="mb-1 flex cursor-pointer space-x-2.5 rounded-md bg-gray-700 px-2 py-1.5 shadow-sm transition-opacity duration-200 ease-in-out"
+				>
+					<FeatherIcon
+						v-for="action in actions"
+						:key="action.label"
+						:name="action.icon"
+						class="h-3.5 w-3.5 text-white"
+						:class="{ hidden: action.hidden && action.hidden(item) }"
+						@click="action.onClick(item)"
+					/>
+				</div>
+			</template>
+		</Popover>
 	</div>
 </template>
