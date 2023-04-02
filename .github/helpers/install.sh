062d648f5b37e6f2dc83f070996858eee20c0368
Saqib Ansari <nextchamp.saqib@gmail.com>
2023-04-02 23:52:21 +0530

    fix: cd into frappe-bench before installing insights

--- .github/helpers/install.sh
+++ .github/helpers/install.sh
@@ -7,11 +7,11 @@ echo "Setting Up Bench..."
 pip install frappe-bench
 git clone "https://github.com/frappe/frappe" --branch "develop" --depth 1
 bench init --skip-assets --frappe-path ~/frappe --python "$(which python)" frappe-bench
-mariadb --host 127.0.0.1 --port 3306 -u root -pfrappe -e "SET GLOBAL character_set_server = 'utf8mb4'";
-mariadb --host 127.0.0.1 --port 3306 -u root -pfrappe -e "SET GLOBAL collation_server = 'utf8mb4_unicode_ci'";
+mariadb --host 127.0.0.1 --port 3306 -u root -proot -e "SET GLOBAL character_set_server = 'utf8mb4'";
+mariadb --host 127.0.0.1 --port 3306 -u root -proot -e "SET GLOBAL collation_server = 'utf8mb4_unicode_ci'";
+cd ~/frappe-bench || exit
 bench get-app insights "${GITHUB_WORKSPACE}" --skip-assets
 bench -v setup requirements
-cd ~/frappe-bench || exit
 
 echo "Setting Up Site..."
 
