6c906c6814bcb7794bb017e7124819a16b8a9b79
Saqib Ansari <nextchamp.saqib@gmail.com>
2023-04-02 23:12:59 +0530

    fix: invalid frappe-bench init path

--- .github/helpers/install.sh
+++ .github/helpers/install.sh
@@ -5,12 +5,13 @@ cd ~ || exit
 echo "Setting Up Bench..."
 
 pip install frappe-bench
-bench -v init frappe-bench --skip-assets --python "$(which python)"
+git clone "https://github.com/frappe/frappe" --branch "develop" --depth 1
+bench init --skip-assets --frappe-path ~/frappe --python "$(which python)" frappe-bench
 mariadb --host 127.0.0.1 --port 3306 -u root -pfrappe -e "SET GLOBAL character_set_server = 'utf8mb4'";
 mariadb --host 127.0.0.1 --port 3306 -u root -pfrappe -e "SET GLOBAL collation_server = 'utf8mb4_unicode_ci'";
 bench get-app insights "${GITHUB_WORKSPACE}" --skip-assets
 bench -v setup requirements
-cd ./frappe-bench || exit
+cd ~/frappe-bench || exit
 
 echo "Setting Up Site..."
 
@@ -18,7 +19,7 @@ bench new-site --db-root-password frappe --admin-password frappe frappe-insights
 bench --site frappe-insights add-to-hosts
 bench --site frappe-insights install-app insights
 bench build
-cp "${GITHUB_WORKSPACE}/.github/helper/mariadb.json" ~/frappe-bench/sites/frappe-insights/site_config.json
+cp "${GITHUB_WORKSPACE}/.github/helpers/mariadb.json" ~/frappe-bench/sites/frappe-insights/site_config.json
 
 echo "Setting Up Procfile..."
 
