db46fdcb9d786da5e71cf7b601a6568bae6411ae
Saqib Ansari <nextchamp.saqib@gmail.com>
2023-04-20 17:49:10 +0530

    fix: get columns from all tables from different data sources

--- insights/insights/doctype/insights_dashboard/insights_dashboard.py
+++ insights/insights/doctype/insights_dashboard/insights_dashboard.py
@@ -120,28 +120,33 @@ class InsightsDashboard(Document):
 @frappe.whitelist()
 def get_queries_column(query_names):
     # TODO: handle permissions
-    tables = {}
+    table_by_datasource = {}
     for query in list(set(query_names)):
         # TODO: to further optimize, store the used tables in the query on save
         doc = frappe.get_cached_doc("Insights Query", query)
         for table in doc.get_selected_tables():
-            tables[table.table] = table
+            if doc.data_source not in table_by_datasource:
+                table_by_datasource[doc.data_source] = {}
+            table_by_datasource[doc.data_source][table.table] = table
 
     columns = []
-    for table in tables.values():
-        doc = frappe.get_cached_doc("Insights Table", {"table": table.table})
-        _columns = doc.get_columns()
-        for column in _columns:
-            columns.append(
-                {
-                    "column": column.column,
-                    "label": column.label,
-                    "table": table.table,
-                    "table_label": table.label,
-                    "type": column.type,
-                    "data_source": doc.data_source,
-                }
+    for data_source in table_by_datasource.values():
+        for table in data_source.values():
+            doc = frappe.get_cached_doc(
+                "Insights Table", {"table": table.table, "data_source": doc.data_source}
             )
+            _columns = doc.get_columns()
+            for column in _columns:
+                columns.append(
+                    {
+                        "column": column.column,
+                        "label": column.label,
+                        "table": table.table,
+                        "table_label": table.label,
+                        "type": column.type,
+                        "data_source": doc.data_source,
+                    }
+                )
 
     return columns
 
