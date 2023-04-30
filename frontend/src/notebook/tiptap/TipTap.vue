1db8e2ca4a3c6705dbebcdf7dadbf53825f24f0c
Saqib Ansari <nextchamp.saqib@gmail.com>
2023-04-30 14:46:46 +0530

    feat: bubble menu

--- frontend/src/notebook/tiptap/TipTap.vue
+++ frontend/src/notebook/tiptap/TipTap.vue
@@ -1,15 +1,40 @@
 <template>
 	<TextEditor
 		:content="$props.content"
-		editorClass="text-base prose-h1:font-semibold prose-h1:my-5 prose-h2:font-semibold prose-h2:my-4 prose-p:text-lg"
+		editorClass="text-base prose-h1:font-semibold prose-h1:my-5 prose-h2:font-semibold prose-h2:my-4 prose-p:text-lg prose-code:before:content-[''] prose-code:after:content-['']"
 		@change="$emit('update:content', $event)"
 		:starterkit-options="{ heading: { levels: [1, 2, 3] } }"
+		:bubble-menu="[
+			'Bold',
+			'Italic',
+			{
+				label: 'Strikethrough',
+				icon: Strikethrough,
+				action: (editor) => editor.chain().focus().toggleStrike().run(),
+				isActive: (editor) => editor.isActive('strike'),
+			},
+			'Blockquote',
+			{
+				label: 'Code',
+				icon: Code,
+				action: (editor) => editor.chain().focus().toggleCode().run(),
+				isActive: (editor) => editor.isActive('code'),
+			},
+			'Link',
+			{
+				label: 'Remove Formatting',
+				icon: RemoveFormatting,
+				action: (editor) => editor.chain().focus().unsetAllMarks().run(),
+				isActive: () => false,
+			},
+		]"
 		:placeholder="placeholderByNode"
 		:extensions="[SlashCommand.configure({ suggestion }), QueryExtension]"
 	></TextEditor>
 </template>
 
 <script setup>
+import { Code, Strikethrough, RemoveFormatting } from 'lucide-vue-next'
 import { TextEditor } from 'frappe-ui'
 import QueryExtension from './extensions/query/QueryExtension'
 import SlashCommand from './slash-command/commands'
@@ -25,7 +50,7 @@ function placeholderByNode({ node }) {
 
 <style lang="scss">
 /* Placeholder */
-[data-placeholder].is-empty::before {
+.prose [data-placeholder].is-empty::before {
 	content: attr(data-placeholder);
 	float: left;
 	pointer-events: none;
@@ -33,5 +58,14 @@ function placeholderByNode({ node }) {
 	align-self: center;
 	font-size: inherit;
 	@apply absolute text-gray-300;
+}
+
+.prose :where(blockquote p:first-of-type):not(:where([class~='not-prose'] *)) {
+	&::before {
+		content: '';
+	}
+	&::after {
+		content: '';
+	}
 }
 </style>
