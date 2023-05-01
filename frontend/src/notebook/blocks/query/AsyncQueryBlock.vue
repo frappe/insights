797e76bf39cd82716dad849b5c95b0544ba29609
Saqib Ansari <nextchamp.saqib@gmail.com>
2023-05-01 17:57:25 +0530

    fix: $listeners not defined

--- frontend/src/notebook/blocks/query/AsyncQueryBlock.vue
+++ frontend/src/notebook/blocks/query/AsyncQueryBlock.vue
@@ -2,15 +2,13 @@
 import QueryBlock from './QueryBlock.vue'
 export default {
 	inheritAttrs: false,
-	components: {
-		QueryBlock,
-	},
+	components: { QueryBlock },
 }
 </script>
 
 <template>
 	<Suspense>
-		<QueryBlock v-bind="{ ...$attrs, ...$props }" v-on="$listeners" />
+		<QueryBlock v-bind="{ ...$attrs, ...$props }" />
 		<template #fallback>
 			<div class="flex h-20 w-full flex-col items-center justify-center">
 				<LoadingIndicator class="mb-2 w-6 text-gray-300" />
