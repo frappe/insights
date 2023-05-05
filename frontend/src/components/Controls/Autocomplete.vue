b992d0049ef262438f2c8b85221eecc6da1a4d32
Saqib Ansari <nextchamp.saqib@gmail.com>
2023-05-05 17:33:22 +0530

    fix: autocomplete blur issue

--- frontend/src/components/Controls/Autocomplete.vue
+++ frontend/src/components/Controls/Autocomplete.vue
@@ -147,7 +147,7 @@ const input = ref(null)
 const popover = ref(null)
 defineExpose({ input })
 
-const blur = () => (input.value.$el.blur(), popover.value.close())
+const blur = () => (input.value.$el.blur(), popover.value.close(), emit('blur'))
 onMounted(() => {
 	if (props.autofocus == false) {
 		setTimeout(blur, 0)
@@ -223,8 +223,9 @@ function handleBlur(event, close) {
 	// and the popover is closed before the list item is selected
 	// so, we need to check if the click was on the list item and prevent closing
 	// closing will be handled by the list item click event
-	if (event.relatedTarget.classList.contains('form-input')) {
-		close()
+	const shouldClose = event.relatedTarget?.classList.contains('form-input')
+	if (shouldClose) {
+		setTimeout(blur, 0)
 	}
 }
 </script>
