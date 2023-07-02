ce01bd5b58caa6c6b2de1cf031667be1fba9e5f1
Saqib Ansari <nextchamp.saqib@gmail.com>
2023-07-03 00:17:25 +0530

    fix: create site db before syncing tables

--- insights/setup/__init__.py
+++ insights/setup/__init__.py
@@ -3,6 +3,7 @@
 
 
 import json
+import os
 import time
 
 import click
@@ -54,5 +55,20 @@ def import_demo_queries_and_dashboards():
 
 
 def sync_site_tables():
+    if frappe.flags.in_test or os.environ.get("CI"):
+        return
+
+    if not frappe.db.exists("Insights Data Source", "Site DB"):
+        create_site_db_data_source()
+
     doc = frappe.get_doc("Insights Data Source", "Site DB")
     doc.enqueue_sync_tables()
+
+
+def create_site_db_data_source():
+    data_source_fixture_path = frappe.get_app_path(
+        "insights", "fixtures", "insights_data_source.json"
+    )
+    with open(data_source_fixture_path, "r") as f:
+        site_db = json.load(f)[0]
+        frappe.get_doc(site_db).insert()
