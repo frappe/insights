9bfaba865facd95dcabc870a5762807776528ecf
Saqib Ansari <nextchamp.saqib@gmail.com>
2023-07-10 11:50:14 +0530

    fix: tooltip doesn't hides on page change

--- frontend/src/components/Charts/ChartTooltip.vue
+++ frontend/src/components/Charts/ChartTooltip.vue
@@ -5,11 +5,10 @@ import { inject, useAttrs } from 'vue'
 
 const options = inject('options')
 
-const { trigger = 'axis', appendToBody = 'true', ...attributes } = useAttrs()
+const { trigger = 'axis', confine = true, ...attributes } = useAttrs()
 
 options.tooltip = {
 	trigger: trigger,
-	appendToBody: appendToBody,
 	...attributes,
 }
 </script>
