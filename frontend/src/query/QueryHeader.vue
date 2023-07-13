f121e4087f219063ee6d600e2116a6b64cbc3008
Saqib Ansari <nextchamp.saqib@gmail.com>
2023-07-13 15:59:52 +0530

    fix: add data source dropdown back

--- frontend/src/query/QueryHeader.vue
+++ frontend/src/query/QueryHeader.vue
@@ -1,7 +1,8 @@
 <script setup lang="jsx">
+import useDataSources from '@/datasource/useDataSources'
 import EditableTitle from '@/query/EditableTitle.vue'
 import QueryMenu from '@/query/QueryMenu.vue'
-import { inject } from 'vue'
+import { computed, inject } from 'vue'
 
 const $notify = inject('$notify')
 const query = inject('query')
@@ -16,11 +17,54 @@ const updateTitle = (title) => {
 		query.doc.title = title
 	})
 }
+
+const sources = useDataSources()
+sources.reload()
+const SourceOption = (props) => {
+	return (
+		<div
+			class="group flex w-full cursor-pointer items-center justify-between rounded-md px-2 py-2 text-sm hover:bg-gray-100"
+			onClick={() => changeDataSource(props.name)}
+		>
+			<span>{props.label}</span>
+			<FeatherIcon v-show={props.active} name="check" class="h-4 w-4 text-gray-600" />
+		</div>
+	)
+}
+const dataSourceOptions = computed(() => {
+	return sources.list.map((source) => ({
+		component: (props) => (
+			<SourceOption
+				name={source.name}
+				label={source.title}
+				active={source.name === query.doc.data_source}
+			/>
+		),
+	}))
+})
+
+function changeDataSource(sourceName) {
+	query.updateDoc({ data_source: sourceName }).then(() => {
+		$notify({
+			title: 'Data source updated',
+			appearance: 'success',
+		})
+		query.doc.data_source = sourceName
+	})
+}
 </script>
 
 <template>
 	<div class="mr-2 flex h-full items-center space-x-2">
 		<EditableTitle :title="query.doc.title" @update="updateTitle" />
+		<Dropdown
+			:button="{
+				iconLeft: 'database',
+				appearance: 'minimal',
+				label: query.doc.data_source || 'Select data source',
+			}"
+			:options="dataSourceOptions"
+		/>
 		<QueryMenu />
 	</div>
 </template>
