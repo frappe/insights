2ef7eb82ba2c5e0a165247dfa804453120544dfa
Saqib Ansari <nextchamp.saqib@gmail.com>
2023-08-19 13:29:12 +0530

    fix: empty states in csv import dialog

--- frontend/src/datasource/FileSourceForm.vue
+++ frontend/src/datasource/FileSourceForm.vue
@@ -105,86 +105,101 @@ function reset() {
 			</template>
 		</FileUploader>
 	</div>
-	<div v-else class="mb-4 flex gap-4">
-		<div class="flex flex-1 flex-col space-y-3">
-			<Input
-				label="New Table Label"
-				type="text"
-				placeholder="eg. Sales Data"
-				v-model="table.label"
-			/>
-			<Input
-				label="New Table Name"
-				type="text"
-				placeholder="eg. sales_data"
-				v-model="table.name"
-			/>
+	<div v-else>
+		<div class="mb-4 flex gap-4">
+			<div class="flex flex-1 flex-col space-y-3">
+				<Input
+					label="New Table Label"
+					type="text"
+					placeholder="eg. Sales Data"
+					v-model="table.label"
+				/>
+				<Input
+					label="New Table Name"
+					type="text"
+					placeholder="eg. sales_data"
+					v-model="table.name"
+				/>
+			</div>
+			<div class="flex flex-1 flex-col space-y-3">
+				<Input
+					type="select"
+					label="Action if exists"
+					v-model="table.ifExists"
+					:options="['Fail', 'Overwrite', 'Append']"
+				/>
+			</div>
 		</div>
-		<div class="flex flex-1 flex-col space-y-3">
-			<Input
-				type="select"
-				label="Action if exists"
-				v-model="table.ifExists"
-				:options="['Fail', 'Overwrite', 'Append']"
-			/>
-		</div>
-	</div>
-	<span v-if="columns?.length" class="text-lg font-medium leading-6 text-gray-900">
-		Columns Mapping
-	</span>
-	<div v-if="columns?.length" class="mt-2 flex max-h-[15rem] flex-col overflow-hidden text-base">
+		<span class="text-base font-medium leading-6 text-gray-900"> Columns Mapping </span>
 		<div
-			class="sticky right-0 top-0 z-10 flex h-8 w-full cursor-pointer items-center space-x-8 pr-8 text-xs uppercase text-gray-600"
+			v-if="columns?.length"
+			class="mt-2 flex max-h-[15rem] flex-col overflow-hidden text-base"
 		>
-			<span class="flex-1"> CSV Column </span>
-			<span class="flex-1"> Column Name </span>
-			<span class="flex-1"> Column Type </span>
-		</div>
-		<div class="flex flex-col overflow-y-scroll">
-			<Draggable class="w-full" v-model="columns" group="columns" item-key="column">
-				<template #item="{ element: column }">
-					<div
-						class="flex h-10 w-full cursor-pointer items-center space-x-8 text-gray-600"
-					>
-						<span
-							class="flex-1 overflow-hidden text-ellipsis whitespace-nowrap text-base font-medium"
-						>
-							{{ column.label }}
-						</span>
-						<Input
-							class="flex-1 !text-sm"
-							type="text"
-							:value="column.name"
-							@change="(name) => (column.name = name)"
-						/>
-						<Input
-							class="flex-1 !text-sm"
-							type="select"
-							:options="columnTypes"
-							:value="column.type"
-							@change="(type) => (column.type = type)"
-						/>
-						<Button
-							class="!ml-0"
-							icon="x"
-							variant="minimal"
-							@click="removeColumn(column.column)"
-						/>
-					</div>
-				</template>
-			</Draggable>
-		</div>
-	</div>
-	<div class="mt-6 flex justify-between pt-2" v-if="table.file">
-		<div class="ml-auto flex items-center space-x-2">
-			<Button
-				variant="solid"
-				@click="submit"
-				:disabled="importDisabled"
-				:loading="importingTable"
+			<div
+				class="sticky right-0 top-0 z-10 flex h-8 w-full cursor-pointer items-center space-x-8 pr-8 pb-2 text-xs uppercase text-gray-600"
 			>
-				Import
-			</Button>
+				<span class="flex-1"> CSV Column </span>
+				<span class="flex-1"> Column Name </span>
+				<span class="flex-1"> Column Type </span>
+			</div>
+			<div class="flex flex-col overflow-y-scroll">
+				<Draggable class="w-full" v-model="columns" group="columns" item-key="column">
+					<template #item="{ element: column }">
+						<div
+							class="flex h-10 w-full cursor-pointer items-center space-x-8 text-gray-600"
+						>
+							<span
+								class="flex-1 overflow-hidden text-ellipsis whitespace-nowrap text-base font-medium"
+							>
+								{{ column.label }}
+							</span>
+							<Input
+								class="flex-1 !text-sm"
+								type="text"
+								:value="column.name"
+								@change="(name) => (column.name = name)"
+							/>
+							<Input
+								class="flex-1 !text-sm"
+								type="select"
+								:options="columnTypes"
+								:value="column.type"
+								@change="(type) => (column.type = type)"
+							/>
+							<Button
+								class="!ml-0"
+								icon="x"
+								variant="minimal"
+								@click="removeColumn(column.column)"
+							/>
+						</div>
+					</template>
+				</Draggable>
+			</div>
+		</div>
+		<div
+			v-else-if="getColumns.loading"
+			class="mt-2 flex flex-col items-center justify-center space-y-2 p-12 text-center text-sm"
+		>
+			<LoadingIndicator class="h-4 w-4 text-gray-500" />
+			<p class="text-gray-600">Loading columns...</p>
+		</div>
+		<div v-else class="mt-2 flex flex-col space-y-3 p-12 text-center text-sm text-gray-600">
+			No columns found in the uploaded file.<br />
+			Please upload a different file.
+		</div>
+
+		<div class="mt-6 flex justify-between pt-2" v-if="table.file">
+			<div class="ml-auto flex items-center space-x-2">
+				<Button
+					variant="solid"
+					@click="submit"
+					:disabled="importDisabled"
+					:loading="importingTable"
+				>
+					Import
+				</Button>
+			</div>
 		</div>
 	</div>
 </template>
