9e3e17126dbb94892b5f7e059d11add09134a729
Saqib Ansari <nextchamp.saqib@gmail.com>
2023-11-05 21:28:23 +0530

    refactor: sort selector

--- frontend/src/new-query-builder/ResultColumnActions.vue
+++ frontend/src/new-query-builder/ResultColumnActions.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ArrowDownUp, ArrowDownWideNarrow, ArrowUpWideNarrow } from 'lucide-vue-next'
+import { ArrowDown, ArrowDownUp, ArrowUp } from 'lucide-vue-next'
 import { inject } from 'vue'
 
 const props = defineProps({ column: Object })
@@ -7,23 +7,32 @@ const builder = inject('builder')
 function getOrder(columnLabel) {
 	return builder.query.columns.find((c) => c.label == columnLabel)?.order
 }
+const sortOptions = [
+	{
+		label: 'Sort Ascending',
+		onClick: () => builder.setOrderBy(column.label, 'asc'),
+	},
+	{
+		label: 'Sort Descending',
+		onClick: () => builder.setOrderBy(column.label, 'desc'),
+	},
+	{
+		label: 'Remove Sort',
+		onClick: () => builder.setOrderBy(column.label, ''),
+	},
+]
+const sortOrderToIcon = {
+	asc: ArrowUp,
+	desc: ArrowDown,
+}
 </script>
 <template>
 	<div class="space-x-1">
-		<ArrowDownUp
-			v-if="!getOrder(column.label)"
-			class="h-4 w-4 cursor-pointer text-gray-500 hover:text-gray-700"
-			@click="builder.setOrderBy(column.label, 'asc')"
-		/>
-		<ArrowUpWideNarrow
-			v-else-if="getOrder(column.label) == 'asc'"
-			class="h-4 w-4 cursor-pointer text-gray-500 hover:text-gray-700"
-			@click="builder.setOrderBy(column.label, 'desc')"
-		/>
-		<ArrowDownWideNarrow
-			v-else-if="getOrder(column.label) == 'desc'"
-			class="h-4 w-4 cursor-pointer text-gray-500 hover:text-gray-700"
-			@click="builder.setOrderBy(column.label, '')"
-		/>
+		<Dropdown :options="sortOptions">
+			<component
+				:is="sortOrderToIcon[getOrder(column.label)] || ArrowDownUp"
+				class="h-4 w-4 cursor-pointer text-gray-500 hover:text-gray-700"
+			/>
+		</Dropdown>
 	</div>
 </template>
