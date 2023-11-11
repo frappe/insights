17a8e40ba892a7940a386bf2ff71ff9f4d43aa30
Saqib Ansari <nextchamp.saqib@gmail.com>
2023-11-12 00:37:32 +0530

    fix: sync only `mariadb` sources

--- insights/insights/doctype/insights_table/patches/sync_table_links.py
+++ insights/insights/doctype/insights_table/patches/sync_table_links.py
@@ -4,7 +4,9 @@ from insights.insights.doctype.insights_data_source.sources.frappe_db import Fra
 
 
 def execute():
-    data_sources = frappe.get_all("Insights Data Source", pluck="name")
+    data_sources = frappe.get_all(
+        "Insights Data Source", {"database_type": "MariaDB"}, pluck="name"
+    )
     for data_source in data_sources:
         doc = frappe.get_doc("Insights Data Source", data_source)
         try:
