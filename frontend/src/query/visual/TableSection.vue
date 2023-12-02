0a16a7532d0ce78ad222934a6683d5a70e4adcf7
Saqib Ansari <nextchamp.saqib@gmail.com>
2023-12-02 15:23:03 +0530

    feat: open query in new tab

--- frontend/src/query/visual/TableSection.vue
+++ frontend/src/query/visual/TableSection.vue
@@ -1,14 +1,15 @@
 <script setup>
 import Autocomplete from '@/components/Controls/Autocomplete.vue'
+import JoinFullIcon from '@/components/Icons/JoinFullIcon.vue'
+import JoinInnerIcon from '@/components/Icons/JoinInnerIcon.vue'
 import JoinLeftIcon from '@/components/Icons/JoinLeftIcon.vue'
 import JoinRightIcon from '@/components/Icons/JoinRightIcon.vue'
-import JoinInnerIcon from '@/components/Icons/JoinInnerIcon.vue'
-import JoinFullIcon from '@/components/Icons/JoinFullIcon.vue'
 import UsePopover from '@/components/UsePopover.vue'
 import useDataSource from '@/datasource/useDataSource'
 import { whenever } from '@vueuse/core'
-import { Sheet, X } from 'lucide-vue-next'
-import { computed, inject, ref, reactive } from 'vue'
+import { ExternalLink, Sheet, X } from 'lucide-vue-next'
+import { computed, inject, reactive, ref } from 'vue'
+import { useRouter } from 'vue-router'
 import TableJoinEditor from './TableJoinEditor.vue'
 
 const assistedQuery = inject('assistedQuery')
@@ -37,6 +38,14 @@ function onRemoveJoin() {
 	assistedQuery.removeJoinAt(activeJoinIdx.value)
 	activeJoinIdx.value = null
 }
+
+const router = useRouter()
+function onTableLinkClick(table) {
+	const route = table.startsWith('QRY-')
+		? router.resolve({ name: 'Query', params: { name: table } })
+		: router.resolve({ name: 'DataSource', params: { name: assistedQuery.data_source } })
+	window.open(route.href, '_blank')
+}
 </script>
 
 <template>
@@ -63,7 +72,13 @@ function onRemoveJoin() {
 				class="group flex h-8 cursor-pointer items-center justify-between rounded border border-gray-300 bg-white px-2 hover:shadow"
 			>
 				<div class="flex items-center space-x-2">
-					<div>{{ assistedQuery.table.label }}</div>
+					<div class="flex flex-1 items-center gap-2">
+						<span class="truncate">{{ assistedQuery.table.label }}</span>
+						<ExternalLink
+							class="h-3 w-3 text-gray-600 opacity-0 transition-all hover:text-gray-800 group-hover:opacity-100"
+							@click.prevent.stop="onTableLinkClick(assistedQuery.table.table)"
+						/>
+					</div>
 				</div>
 				<div class="flex items-center space-x-2">
 					<X
@@ -84,7 +99,13 @@ function onRemoveJoin() {
 				"
 				@click="activeJoinIdx = idx"
 			>
-				<div>{{ join.right_table.label }}</div>
+				<div class="flex flex-1 items-center gap-2">
+					<span class="truncate">{{ join.right_table.label }}</span>
+					<ExternalLink
+						class="h-3 w-3 text-gray-600 opacity-0 transition-all hover:text-gray-800 group-hover:opacity-100"
+						@click.prevent.stop="onTableLinkClick(join.right_table.table)"
+					/>
+				</div>
 				<JoinLeftIcon v-if="join.join_type.value == 'left'" class="text-gray-600" />
 				<JoinRightIcon v-if="join.join_type.value == 'right'" class="text-gray-600" />
 				<JoinInnerIcon v-if="join.join_type.value == 'inner'" class="text-gray-600" />
@@ -100,7 +121,7 @@ function onRemoveJoin() {
 		@update:show="activeJoinIdx = null"
 		:target-element="joinRefs[activeJoinIdx]"
 	>
-		<div class="w-[24rem] rounded bg-white text-base shadow-2xl">
+		<div class="min-w-[24rem] rounded bg-white text-base shadow-2xl">
 			<TableJoinEditor
 				:join="joins[activeJoinIdx]"
 				@save="onSaveJoin($event)"
