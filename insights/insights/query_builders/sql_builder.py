518239a0ec892f42d659d1a31331b2b3b9402be8
Gavin D'souza <gavin.dsouza@switchup.de>
2024-03-14 13:03:50 +0100

    fix: PostgresColumnFormatter.format_date
    The base class defined MySQL based date formatting utils which were not
    supported in Postgres.
    
    Note: The formats may not be exact equivalents, I don't have a setup to
    test different database types with similar reports currently
    
    ref: https://www.postgresql.org/docs/current/functions-formatting.html

--- insights/insights/query_builders/sql_builder.py
+++ insights/insights/query_builders/sql_builder.py
@@ -2,10 +2,10 @@ from typing import List
 
 import frappe
 from frappe import _dict
-from sqlalchemy import TextClause
+from sqlalchemy import Column, TextClause
 from sqlalchemy import column as sa_column
 from sqlalchemy import select, table
-from sqlalchemy.sql import and_, text
+from sqlalchemy.sql import and_, func, text
 
 from insights.insights.doctype.insights_query.utils import Column as AssistedQueryColumn
 from insights.insights.doctype.insights_query.utils import Filter as AssistedQueryFilter
@@ -24,12 +24,55 @@ from .sql_functions import (
 from .utils import process_raw_expression
 
 
+class PostgresColumnFormatter(ColumnFormatter):
+    @classmethod
+    def format_date(cls, format, column: Column):
+        if format == "Minute":
+            return func.to_char(column, "YYYY-MM-DD HH24:MI")
+        if format == "Hour":
+            return func.to_char(column, "YYYY-MM-DD HH24:00")
+        if format == "Day" or format == "Day Short":
+            return func.to_char(column, "YYYY-MM-DD 00:00")
+        if format == "Week":
+            return func.date_trunc("week", column)
+        if format == "Month" or format == "Mon":
+            return func.to_char(column, "YYYY-MM-01")
+        if format == "Year":
+            return func.to_char(column, "YYYY-01-01")
+        if format == "Minute of Hour":
+            return func.to_char(column, "00:MI")
+        if format == "Hour of Day":
+            return func.to_char(column, "HH:00")
+        if format == "Day of Week":
+            return func.to_char(column, "Day")
+        if format == "Day of Month":
+            return func.to_char(column, "DD")
+        if format == "Day of Year":
+            return func.to_char(column, "DDD")
+        if format == "Month of Year":
+            return func.to_char(column, "MM")
+        if format == "Quarter of Year":
+            return func.date_part("quarter", column)
+        if format == "Quarter":
+            return func.to_date(
+                func.concat(
+                    func.extract("year", column),
+                    "-",
+                    (func.extract("quarter", column) * 3) - 2,
+                    "-01",
+                ),
+                "YYYY-MM-DD",
+            )
+        else:
+            return func.to_char(column, format)
+
+
 class SQLQueryBuilder:
     def __init__(self, engine) -> None:
         self.engine = engine
         self.functions = Functions
         self.aggregations = Aggregations
-        self.column_formatter = ColumnFormatter
+        self.column_formatter = PostgresColumnFormatter
 
     def build(self, query):
         if query.is_native_query:
