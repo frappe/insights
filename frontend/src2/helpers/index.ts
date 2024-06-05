30f4b396a35f533170945f289148ce088c3cc069
Saqib Ansari <nextchamp.saqib@gmail.com>
2024-06-06 00:13:37 +0530

    chore: `waitUntil` helper

--- frontend/src2/helpers/index.ts
+++ frontend/src2/helpers/index.ts
@@ -1,3 +1,5 @@
+import { watch } from 'vue'
+
 export function getUniqueId(length = 6) {
 	return (+new Date() * Math.random()).toString(36).substring(0, length)
 }
@@ -14,4 +16,19 @@ export function titleCase(str: string) {
 
 export function copy(obj: object) {
 	return JSON.parse(JSON.stringify(obj))
+}
+
+export function waitUntil(fn: () => boolean) {
+	return new Promise<void>((resolve) => {
+		if (fn()) {
+			resolve()
+			return
+		}
+		const stop = watch(fn, (value) => {
+			if (value) {
+				stop()
+				resolve()
+			}
+		})
+	})
 }
