8832b66f385c719dfd1d21f97779674b483e738d
Saqib Ansari <nextchamp.saqib@gmail.com>
2024-06-06 00:12:50 +0530

    chore: show unsaved badge

--- frontend/src2/workbook/Workbook.vue
+++ frontend/src2/workbook/Workbook.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import ContentEditable from '@/components/ContentEditable.vue'
+import { Badge } from 'frappe-ui'
 import { provide, watch, watchEffect } from 'vue'
 import ChartBuilder from '../charts/ChartBuilder.vue'
 import Navbar from '../components/Navbar.vue'
@@ -36,14 +37,26 @@ watchEffect(() => {
 	<div class="flex h-full w-full flex-col">
 		<Navbar>
 			<template #left>
-				<ContentEditable
-					class="rounded-sm text-lg font-medium !text-gray-800 focus:ring-2 focus:ring-gray-700 focus:ring-offset-4"
-					v-model="workbook.doc.title"
-					placeholder="Untitled Dashboard"
-				></ContentEditable>
+				<div class="flex gap-3">
+					<ContentEditable
+						class="rounded-sm text-lg font-medium !text-gray-800 focus:ring-2 focus:ring-gray-700 focus:ring-offset-4"
+						v-model="workbook.doc.title"
+						placeholder="Untitled Workbook"
+					></ContentEditable>
+					<Badge v-if="workbook.islocal || workbook.isdirty" theme="orange">
+						Unsaved
+					</Badge>
+				</div>
 			</template>
 			<template #actions>
 				<div class="flex gap-2">
+					<Button
+						v-show="!workbook.islocal && workbook.isdirty"
+						variant="outline"
+						@click="workbook.discard()"
+					>
+						Discard
+					</Button>
 					<Button
 						v-show="workbook.islocal || workbook.isdirty"
 						variant="solid"
