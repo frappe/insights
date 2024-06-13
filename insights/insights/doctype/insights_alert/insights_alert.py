22c45590153f80f08f4a4a8eb3e9a66dfe990c9b
Saqib Ansari <nextchamp.saqib@gmail.com>
2024-06-13 17:26:21 +0530

    fix: don't break alert if results are empty

--- insights/insights/doctype/insights_alert/insights_alert.py
+++ insights/insights/doctype/insights_alert/insights_alert.py
@@ -5,7 +5,6 @@ from datetime import datetime
 import frappe
 import telegram
 from croniter import croniter
-from frappe.integrations.utils import make_post_request
 from frappe.model.document import Document
 from frappe.utils import validate_email_address
 from frappe.utils.data import get_datetime, get_datetime_str, now_datetime
@@ -47,20 +46,22 @@ class InsightsAlert(Document):
 
     def evaluate_condition(self, for_validate=False):
         query = frappe.get_doc("Insights Query", self.query)
-        results = query.fetch_results() if not for_validate else query.retrieve_results()
+        results = query.retrieve_results(fetch_if_not_cached=not for_validate)
 
-        if not results:
+        if not results.empty:
             return False
 
         column_names = [d.get("label") for d in results[0]]
         results = DataFrame(results[1:], columns=column_names)
 
-        return frappe.safe_eval(self.condition, eval_locals=frappe._dict(results=results, any=any))
+        return frappe.safe_eval(
+            self.condition, eval_locals=frappe._dict(results=results, any=any)
+        )
 
     def evaluate_message(self):
         query = frappe.get_doc("Insights Query", self.query)
         query_dict = query.as_dict()
-        # query_dict.results = f"""<div class="results">{query.get_formatted_results(as_html=True)}</div>"""
+        query_dict.results = query.retrieve_results(fetch_if_not_cached=True)
         message = frappe.render_template(self.message, context=query_dict)
         return frappe.render_template(
             "insights/templates/alert.html", context=frappe._dict(message=message)
@@ -109,7 +110,9 @@ def send_alerts():
 
 class Telegram:
     def __init__(self, chat_id: str = None):
-        self.token = frappe.get_single("Insights Settings").get_password("telegram_api_token")
+        self.token = frappe.get_single("Insights Settings").get_password(
+            "telegram_api_token"
+        )
         if not self.token:
             frappe.throw("Telegram Bot Token not set in Insights Settings")
 
@@ -120,7 +123,9 @@ class Telegram:
         try:
             text = message[: telegram.MAX_MESSAGE_LENGTH]
             parse_mode = telegram.ParseMode.MARKDOWN
-            return self.bot.send_message(chat_id=self.chat_id, text=text, parse_mode=parse_mode)
+            return self.bot.send_message(
+                chat_id=self.chat_id, text=text, parse_mode=parse_mode
+            )
         except Exception:
             frappe.log_error("Telegram Bot Error")
             raise
