983dfdb142d499f4e23a36dd69d2e828410c64f2
Saqib Ansari <nextchamp.saqib@gmail.com>
2024-06-24 21:38:08 +0530

    fix: handle join on unmatching columns type

--- insights/insights/doctype/insights_data_source_v3/ibis_utils.py
+++ insights/insights/doctype/insights_data_source_v3/ibis_utils.py
@@ -73,9 +73,10 @@ class IbisQueryBuilder:
         right_table = self.get_table(join_args.table)
         left_column = getattr(_, join_args.left_column.column_name)
         right_column = getattr(right_table, join_args.right_column.column_name)
-        join_on = left_column == right_column
+        join_on = left_column.cast(right_column.type()) == right_column
         join_type = join_args.join_type
         join_type = "outer" if join_type == "full" else join_type
+
         return lambda query: query.join(right_table, join_on, how=join_type).select(
             ~s.endswith("right")
         )
