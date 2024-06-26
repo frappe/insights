4e0b18f1b65bc5903f2e86de6ef0bb4c7f873b23
Saqib Ansari <nextchamp.saqib@gmail.com>
2024-06-26 22:37:39 +0530

    fix: remove unused code

--- frontend/src2/dashboard/DashboardItem.vue
+++ frontend/src2/dashboard/DashboardItem.vue
@@ -55,38 +55,6 @@ function handleApplyFilter(operator: FilterOperator, value: FilterValue) {
 	>
 		<div class="h-full w-full" :class="dashboard.editing ? 'pointer-events-none' : ''">
 			<ChartRenderer v-if="chart" :chart="chart" />
-
-			<div v-if="filter" class="flex-1">
-				<ColumnFilter
-					placement="bottom-start"
-					:column="filter.column"
-					:valuesProvider="getDistinctColumnValues"
-					@filter="handleApplyFilter"
-				>
-					<template #target="{ togglePopover, isOpen }">
-						<div class="relative flex-1">
-							<Button
-								variant="outline"
-								@click="togglePopover"
-								class="h-8 w-full !justify-start border-0 shadow"
-							>
-								<template #prefix>
-									<DataTypeIcon :column-type="filter.column.type" />
-								</template>
-								{{ filter.column.name }}
-							</Button>
-							<div class="absolute top-0 right-0.5 flex h-full items-center">
-								<Button
-									variant="ghost"
-									:disabled="!isOpen"
-									:icon="isOpen ? 'x' : 'chevron-down'"
-									@click=""
-								/>
-							</div>
-						</div>
-					</template>
-				</ColumnFilter>
-			</div>
 		</div>
 	</div>
 </template>
