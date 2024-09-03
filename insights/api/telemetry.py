73f405d5fc37fffb18ad1182cf8f8f138eb54fc9
Saqib Ansari <nextchamp.saqib@gmail.com>
2024-09-03 12:08:56 +0530

    refactor: use posthog credentials from `site_config`

--- insights/api/telemetry.py
+++ insights/api/telemetry.py
@@ -7,48 +7,41 @@ import frappe
 from frappe.utils.data import date_diff
 from posthog import Posthog
 
-try:
-    from frappe.utils.telemetry import capture
-except ImportError:
-
-    def capture(*args, **kwargs):
-        pass
-
-
-@frappe.whitelist()
-def is_enabled():
-    return (
-        frappe.get_system_settings("enable_telemetry")
-        and frappe.conf.get("posthog_project_id")
-        and frappe.conf.get("posthog_host")
-    )
-
 
 @frappe.whitelist()
 def get_credentials():
     return {
         "project_id": frappe.conf.get("posthog_project_id"),
-        "telemetry_host": frappe.conf.get("posthog_host"),
+        "host": frappe.conf.get("posthog_host"),
     }
-
-
-def track(event):
-    return capture(event, "insights")
 
 
 @frappe.whitelist()
 def track_active_site():
     is_frappe_cloud_site = frappe.conf.get("sk_insights")
-    if frappe.conf.developer_mode or not should_track_active_status() or not is_frappe_cloud_site:
+    if (
+        frappe.conf.developer_mode
+        or not should_track_active_status()
+        or not is_frappe_cloud_site
+    ):
+        return
+
+    capture_event("insights_active_site")
+    frappe.cache().set_value("last_active_at", frappe.utils.now_datetime())
+
+
+def capture_event(event_name, properties=None):
+    if not frappe.conf.get("posthog_project_id") or not frappe.conf.get("posthog_host"):
         return
 
     with suppress(Exception):
         ph = Posthog(
-            "phc_PxMKOBaHDGJApbZkYqSVro6YSecTYgQ6tB4BAV2nYmd",
-            host="https://posthog.frappe.cloud",
+            frappe.conf.get("posthog_project_id"),
+            host=frappe.conf.get("posthog_host"),
         )
-        ph.capture(distinct_id=frappe.local.site, event="insights_active_site")
-        frappe.cache().set_value("last_active_at", frappe.utils.now_datetime())
+        ph.capture(
+            distinct_id=frappe.local.site, event=event_name, properties=properties
+        )
 
 
 def should_track_active_status():
