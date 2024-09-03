859b5924d03e0417173f00f4ff8ab20765f4f00b
pu-raihan <raihanbinmary@gmail.com>
2024-09-03 16:01:59 +0530

    fix(ux): hide sidebar before being mounted

--- frontend/src/components/AppShell.vue
+++ frontend/src/components/AppShell.vue
@@ -19,11 +19,18 @@ import Sidebar from '@/components/Sidebar.vue'
 import SuspenseFallback from '@/components/SuspenseFallback'
 import sessionStore from '@/stores/sessionStore'
 import settingsStore from '@/stores/settingsStore'
-import { computed, onMounted } from 'vue'
+import { ref, onMounted, watch } from 'vue'
 import { useRoute } from 'vue-router'
 
 const session = sessionStore()
 const route = useRoute()
-const hideSidebar = computed(() => route.meta.hideSidebar || !session.isLoggedIn)
+const hideSidebar = ref(true)
+watch(
+	route,
+	(newRoute) => {
+		hideSidebar.value = newRoute.meta.hideSidebar || !session.isLoggedIn
+	},
+	{ immediate: true }
+)
 onMounted(() => session.isLoggedIn && settingsStore())
 </script>
