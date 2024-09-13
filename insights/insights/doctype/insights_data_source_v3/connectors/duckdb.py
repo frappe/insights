e10129f4f4d387969ebbcc7f2d23e5bf85855aee
Saqib Ansari <nextchamp.saqib@gmail.com>
2024-09-13 12:43:07 +0530

    chore: remove unused method

--- insights/insights/doctype/insights_data_source_v3/connectors/duckdb.py
+++ insights/insights/doctype/insights_data_source_v3/connectors/duckdb.py
@@ -3,31 +3,7 @@
 
 import os
 
-import frappe
-import pandas as pd
 from frappe.utils import get_files_path
-
-from insights.utils import detect_encoding
-
-from ...insights_table_import.insights_table_import import InsightsTableImport
-
-
-def import_table(self, import_doc: InsightsTableImport):
-    encoding = detect_encoding(import_doc._filepath)
-    df = pd.read_csv(import_doc._filepath, encoding=encoding)
-
-    df.columns = [frappe.scrub(c) for c in df.columns]
-    columns_to_import = [c.column for c in import_doc.columns]
-
-    df = df[columns_to_import]
-    table = import_doc.table_name
-    df.to_sql(
-        name=table,
-        con=self.engine,
-        index=False,
-        if_exists="replace",
-    )
-    # create_insights_table()
 
 
 def get_duckdb_connection_string(data_source):
