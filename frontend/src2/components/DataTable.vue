7058d89986547f34b3d4835bc233a24a1ff66f03
Saqib Ansari <nextchamp.saqib@gmail.com>
2024-10-23 14:11:20 +0530

    fix: minor datatable fixes

--- frontend/src2/components/DataTable.vue
+++ frontend/src2/components/DataTable.vue
@@ -104,7 +104,7 @@ const filterPerColumn = ref<Record<string, string>>({})
 		class="flex h-full w-full flex-col overflow-hidden font-mono text-sm"
 	>
 		<div class="w-full flex-1 overflow-y-auto">
-			<table class="h-full w-full border-separate border-spacing-0">
+			<table class="relative h-full w-full border-separate border-spacing-0">
 				<thead class="sticky top-0 z-10 bg-white">
 					<tr>
 						<td class="whitespace-nowrap border-b border-r" width="1%"></td>
@@ -129,9 +129,7 @@ const filterPerColumn = ref<Record<string, string>>({})
 							<div class="truncate pl-3 pr-20"></div>
 						</td>
 					</tr>
-				</thead>
-				<tbody>
-					<!-- filter row -->
+
 					<tr v-if="props.showFilterRow">
 						<td class="whitespace-nowrap border-b border-r" width="1%"></td>
 						<td
@@ -149,6 +147,8 @@ const filterPerColumn = ref<Record<string, string>>({})
 							<div class="truncate pl-3 pr-20"></div>
 						</td>
 					</tr>
+				</thead>
+				<tbody>
 					<tr v-for="(row, idx) in visibleRows?.slice(0, 100)" :key="idx">
 						<td
 							class="whitespace-nowrap border-b border-r px-3"
@@ -157,6 +157,7 @@ const filterPerColumn = ref<Record<string, string>>({})
 						>
 							{{ idx + 1 }}
 						</td>
+
 						<td
 							v-for="col in props.columns"
 							class="max-w-[24rem] truncate border-b border-r py-2 px-3 text-gray-800"
@@ -176,11 +177,14 @@ const filterPerColumn = ref<Record<string, string>>({})
 						</td>
 					</tr>
 
-					<tr v-if="props.showColumnTotals && totalPerColumn" class="border-b">
-						<td class="whitespace-nowrap border-r px-3"></td>
+					<tr
+						v-if="props.showColumnTotals && totalPerColumn"
+						class="sticky bottom-0 z-10 border-b bg-white"
+					>
+						<td class="whitespace-nowrap border-r border-t px-3"></td>
 						<td
 							v-for="col in props.columns"
-							class="truncate border-r py-2 px-3 font-bold text-gray-800"
+							class="truncate border-r border-t py-2 px-3 font-bold text-gray-800"
 							:class="isNumberColumn(col) ? 'text-right' : 'text-left'"
 						>
 							{{ isNumberColumn(col) ? formatNumber(totalPerColumn[col.name]) : '' }}
@@ -188,7 +192,7 @@ const filterPerColumn = ref<Record<string, string>>({})
 
 						<td
 							v-if="props.showRowTotals && totalColumnTotal"
-							class="border-r px-3 text-right font-bold"
+							class="border-r border-t px-3 text-right font-bold"
 						>
 							{{ formatNumber(totalColumnTotal) }}
 						</td>
