440b9393d5fcc946e0945ebb71a44c40e832729a
Saqib Ansari <nextchamp.saqib@gmail.com>
2024-11-07 15:54:24 +0530

    fix: encode passwords in connection strings

--- insights/insights/doctype/insights_data_source_v3/connectors/mariadb.py
+++ insights/insights/doctype/insights_data_source_v3/connectors/mariadb.py
@@ -1,9 +1,11 @@
 # Copyright (c) 2022, Frappe Technologies Pvt. Ltd. and contributors
 # For license information, please see license.txt
+from urllib.parse import quote_plus
 
 
 def get_mariadb_connection_string(data_source):
     password = data_source.get_password(raise_exception=False)
+    password = quote_plus(password) if password else ""
     connection_string = (
         f"mysql://{data_source.username}:{password}"
         f"@{data_source.host}:{data_source.port}/{data_source.database_name}"
--- insights/insights/doctype/insights_data_source_v3/connectors/postgresql.py
+++ insights/insights/doctype/insights_data_source_v3/connectors/postgresql.py
@@ -2,11 +2,16 @@
 # For license information, please see license.txt
 
 
+from urllib.parse import quote_plus
+
+
 def get_postgres_connection_string(data_source):
     if data_source.connection_string:
-        return data_source.connection_string
+        conn_string = quote_plus(data_source.connection_string)
+        return conn_string
     else:
         password = data_source.get_password(raise_exception=False)
+        password = quote_plus(password) if password else ""
         connection_string = (
             f"postgresql://{data_source.username}:{password}"
             f"@{data_source.host}:{data_source.port}/{data_source.database_name}"
