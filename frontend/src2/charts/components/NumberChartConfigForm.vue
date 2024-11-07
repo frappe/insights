0cc144cb05b8444494ae0b8dcc8c377b61781e1c
Saqib Ansari <nextchamp.saqib@gmail.com>
2024-11-07 15:39:10 +0530

    feat: re-order measures & dimensions in number & table chart

--- frontend/src2/charts/components/NumberChartConfigForm.vue
+++ frontend/src2/charts/components/NumberChartConfigForm.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
-import { FIELDTYPES } from '../../helpers/constants'
-import { computed, watchEffect } from 'vue'
+import ColorInput from '@/components/Controls/ColorInput.vue'
 import { debounce } from 'frappe-ui'
+import { computed, watchEffect } from 'vue'
 import InlineFormControlLabel from '../../components/InlineFormControlLabel.vue'
+import { copy } from '../../helpers'
+import { FIELDTYPES } from '../../helpers/constants'
 import { NumberChartConfig } from '../../types/chart.types'
 import { DimensionOption, MeasureOption } from './ChartConfigForm.vue'
 import CollapsibleSection from './CollapsibleSection.vue'
-import { Settings, XIcon } from 'lucide-vue-next'
-import { copy } from '../../helpers'
-import ColorInput from '@/components/Controls/ColorInput.vue'
+import MeasurePicker from './MeasurePicker.vue'
 
 const props = defineProps<{
 	dimensions: DimensionOption[]
@@ -49,72 +49,10 @@ const updateColor = debounce((color: string) => {
 <template>
 	<CollapsibleSection title="Options">
 		<div class="flex flex-col gap-3 pt-1">
-			<template v-for="(c, idx) in config.number_columns" :key="idx">
-				<div class="flex items-end gap-1">
-					<div class="flex-1">
-						<Autocomplete
-							placeholder="Select a column"
-							:label="
-								config.number_columns.length > 1 ? `Column ${idx + 1}` : 'Column'
-							"
-							:showFooter="true"
-							:options="props.measures"
-							:modelValue="c.measure_name"
-							@update:modelValue="Object.assign(c, $event || {})"
-						/>
-					</div>
-					<Popover v-if="c.measure_name" placement="bottom-end">
-						<template #target="{ togglePopover }">
-							<Button @click="togglePopover">
-								<template #icon>
-									<Settings class="h-4 w-4 text-gray-700" stroke-width="1.5" />
-								</template>
-							</Button>
-						</template>
-						<template #body-main>
-							<div class="flex w-[14rem] flex-col gap-2 p-2">
-								<InlineFormControlLabel label="Label">
-									<FormControl
-										v-model="c.measure_name"
-										autocomplete="off"
-										:debounce="500"
-									/>
-								</InlineFormControlLabel>
-								<div class="flex gap-1">
-									<Button
-										class="w-full"
-										@click="config.number_columns.splice(idx, 1)"
-										theme="red"
-									>
-										<template #prefix>
-											<XIcon
-												class="h-4 w-4 text-red-700"
-												stroke-width="1.5"
-											/>
-										</template>
-										Remove
-									</Button>
-								</div>
-							</div>
-						</template>
-					</Popover>
-					<Button
-						v-else
-						class="flex-shrink-0"
-						@click="config.number_columns.splice(idx, 1)"
-					>
-						<template #icon>
-							<XIcon class="h-4 w-4 text-gray-700" stroke-width="1.5" />
-						</template>
-					</Button>
-				</div>
-			</template>
-			<button
-				class="-mt-1 text-left text-xs text-gray-600 hover:underline"
-				@click="addNumberColumn"
-			>
-				+ Add column
-			</button>
+			<div>
+				<p class="mb-1.5 text-xs text-gray-600">Columns</p>
+				<MeasurePicker v-model="config.number_columns" :options="props.measures" />
+			</div>
 
 			<InlineFormControlLabel label="Date">
 				<Autocomplete
