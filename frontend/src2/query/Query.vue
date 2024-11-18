2334e3b41f47fe53e1f3c7310b633a63a9348455
Saqib Ansari <nextchamp.saqib@gmail.com>
2024-11-18 14:28:52 +0530

    feat: table & column completions in sql editor

--- frontend/src2/query/Query.vue
+++ frontend/src2/query/Query.vue
@@ -1,27 +1,15 @@
 <script setup lang="ts">
-import { useMagicKeys, whenever } from '@vueuse/core'
-import { onBeforeUnmount, provide } from 'vue'
+import { provide } from 'vue'
 import { WorkbookQuery } from '../types/workbook.types'
 import NativeQueryEditor from './components/NativeQueryEditor.vue'
-import useQuery from './query'
 import QueryBuilder from './components/QueryBuilder.vue'
+import useQuery from './query'
 
 const props = defineProps<{ query: WorkbookQuery }>()
 const query = useQuery(props.query)
 provide('query', query)
 window.query = query
 query.execute()
-
-const keys = useMagicKeys()
-const cmdZ = keys['Meta+Z']
-const cmdShiftZ = keys['Meta+Shift+Z']
-const stopUndoWatcher = whenever(cmdZ, () => query.canUndo() && query.history.undo())
-const stopRedoWatcher = whenever(cmdShiftZ, () => query.canRedo() && query.history.redo())
-
-onBeforeUnmount(() => {
-	stopUndoWatcher()
-	stopRedoWatcher()
-})
 </script>
 
 <template>
