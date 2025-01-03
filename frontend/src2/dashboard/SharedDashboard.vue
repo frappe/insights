a8be216c58628dacdf5b3b05eaf0ffcd5d65f8e2
NAGARJUN V <nagarjun063@gmail.com>
2025-01-03 18:22:50 +0530

    Filtering Shared Dashboard

--- frontend/src2/dashboard/SharedDashboard.vue
+++ frontend/src2/dashboard/SharedDashboard.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { call } from 'frappe-ui'
 import { ref } from 'vue'
+import { useRoute } from 'vue-router'
 import SharedChart from '../charts/SharedChart.vue'
 import { WorkbookDashboard } from '../types/workbook.types'
 import VueGridLayout from './VueGridLayout.vue'
@@ -8,6 +9,19 @@ import VueGridLayout from './VueGridLayout.vue'
 const props = defineProps<{ dashboard_name: string }>()
 
 const dashboard = ref<WorkbookDashboard>()
+	const route = useRoute();
+const queryString = new URLSearchParams(route.query);
+const keyValuePairs: Record<string, string[]> = {};
+
+Object.entries(route.query).forEach(([key, value]) => {
+  if (Array.isArray(value)) {
+    keyValuePairs[key] = value
+  } else {
+    keyValuePairs[key] = [value]
+  }
+})
+
+
 
 dashboard.value = await call('insights.api.dashboards.fetch_workbook_dashboard', {
 	dashboard_name: props.dashboard_name,
@@ -26,7 +40,9 @@ dashboard.value = await call('insights.api.dashboards.fetch_workbook_dashboard',
 			>
 				<template #item="{ index }">
 					<div class="relative h-full w-full rounded p-2">
-						<SharedChart :chart_name="dashboard.items[index].chart" />
+						<SharedChart :chart_name="dashboard.items[index].chart"
+						:query="keyValuePairs"
+						/>
 					</div>
 				</template>
 			</VueGridLayout>
