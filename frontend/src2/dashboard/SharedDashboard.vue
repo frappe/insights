95a3c51b380e3271205be207b8b8db81396c2858
NAGARJUN V <nagarjun063@gmail.com>
2025-01-09 18:32:06 +0530

    Update SharedDashboard.vue

--- frontend/src2/dashboard/SharedDashboard.vue
+++ frontend/src2/dashboard/SharedDashboard.vue
@@ -9,16 +9,16 @@ import VueGridLayout from './VueGridLayout.vue'
 const props = defineProps<{ dashboard_name: string }>()
 
 const dashboard = ref<WorkbookDashboard>()
-	const route = useRoute();
+const route = useRoute();
 const queryString = new URLSearchParams(route.query);
 const keyValuePairs: Record<string, string[]> = {};
 
 Object.entries(route.query).forEach(([key, value]) => {
-  if (Array.isArray(value)) {
-    keyValuePairs[key] = value
-  } else {
-    keyValuePairs[key] = [value]
-  }
+	if (Array.isArray(value)) {
+		keyValuePairs[key] = value
+	} else {
+		keyValuePairs[key] = [value]
+	}
 })
 
 
@@ -31,21 +31,19 @@ dashboard.value = await call('insights.api.dashboards.fetch_workbook_dashboard',
 <template>
 	<div class="relative flex h-full w-full overflow-hidden">
 		<div class="flex-1 overflow-y-auto p-4">
-			<VueGridLayout
-				v-if="dashboard && dashboard.items.length > 0"
-				class="h-fit w-full"
-				:cols="20"
-				:disabled="true"
-				:modelValue="dashboard.items.map((item) => item.layout)"
-			>
-				<template #item="{ index }">
-					<div class="relative h-full w-full rounded p-2">
-						<SharedChart :chart_name="dashboard.items[index].chart"
-						:query="keyValuePairs"
-						/>
-					</div>
-				</template>
-			</VueGridLayout>
+			<div v-if="dashboard && dashboard.is_public">
+				<VueGridLayout v-if="dashboard && dashboard.items.length > 0" class="h-fit w-full" :cols="20"
+					:disabled="true" :modelValue="dashboard.items.map((item) => item.layout)">
+					<template #item="{ index }">
+						<div class="relative h-full w-full rounded p-2">
+							<SharedChart :chart_name="dashboard.items[index].chart" :query="keyValuePairs" />
+						</div>
+					</template>
+				</VueGridLayout>
+			</div>
+			<div v-else class="flex-1 flex items-center justify-center">
+				<p class="text-red-500">Access Denied: Public Sharing is Disabled</p>
+			</div>
 		</div>
 	</div>
 </template>
