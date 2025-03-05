f9fe4c52ef5ee8eba76b6775f6fd8c90cb2ab6c3
Saqib Ansari <nextchamp.saqib@gmail.com>
2025-03-05 16:41:37 +0530

    chore: remove unused methods

--- insights/api/shared.py
+++ insights/api/shared.py
@@ -1,7 +1,5 @@
 import frappe
-from frappe.utils.caching import redis_cache
 
-from insights.api.data_sources import fetch_column_values
 from insights.decorators import validate_type
 
 
@@ -100,69 +98,3 @@ def is_shared_query(name: str):
         return True
 
     return False
-
-
-@frappe.whitelist(allow_guest=True)
-def get_public_chart(public_key):
-    if not public_key or not isinstance(public_key, str):
-        frappe.throw("Public Key is required")
-
-    chart_name = frappe.db.exists(
-        "Insights Chart", {"public_key": public_key, "is_public": 1}
-    )
-    if not chart_name:
-        frappe.throw("Invalid Public Key")
-
-    chart = frappe.get_cached_doc("Insights Chart", chart_name).as_dict(
-        no_default_fields=True
-    )
-    chart_data = frappe.get_cached_doc("Insights Query", chart.query).fetch_results()
-    chart["data"] = chart_data
-    return chart
-
-
-@frappe.whitelist(allow_guest=True)
-def get_public_dashboard_chart_data(public_key, *args, **kwargs):
-    if not public_key or not isinstance(public_key, str):
-        frappe.throw("Public Key is required")
-
-    dashboard_name = frappe.db.exists(
-        "Insights Dashboard", {"public_key": public_key, "is_public": 1}
-    )
-    if not dashboard_name:
-        frappe.throw("Invalid Public Key")
-
-    kwargs.pop("cmd")
-    return frappe.get_cached_doc("Insights Dashboard", dashboard_name).fetch_chart_data(
-        *args, **kwargs
-    )
-
-
-@frappe.whitelist(allow_guest=True)
-@redis_cache()
-def fetch_column_values_public(public_key, item_id, search_text=None):
-    if not public_key or not isinstance(public_key, str):
-        frappe.throw("Public Key is required")
-
-    dashboard_name = frappe.db.exists(
-        "Insights Dashboard", {"public_key": public_key, "is_public": 1}
-    )
-    if not dashboard_name:
-        frappe.throw("Invalid Public Key")
-
-    doc = frappe.get_doc("Insights Dashboard", dashboard_name)
-    row = next((row for row in doc.items if row.item_id == item_id), None)
-    if not row:
-        frappe.throw("Invalid Item ID")
-
-    options = frappe.parse_json(row.options)
-    column = options.get("column")
-    if not column:
-        frappe.throw("Column not found in Item Options")
-
-    return fetch_column_values(
-        data_source=column.get("data_source"),
-        table=column.get("table"),
-        column=column.get("column"),
-        search_text=search_text,
-    )
