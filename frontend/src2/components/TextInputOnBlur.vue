0aec408f9fada088c97a8903207d2edeef3bb8da
shahzeelahmed <ahmedshahzeel@gmail.com>
2025-06-23 15:18:41 -0700

    feat: add TextInputOnBlur component

---
+++ frontend/src2/components/TextInputOnBlur.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { TextInput } from 'frappe-ui';
+
+const modelValue = defineModel({
+    type: String,
+    required: true,
+    default: '',
+});
+
+</script>
+
+<template>
+    <TextInput
+        v-bind="$attrs"
+        type="text"
+        class="w-full"
+        variant="subtle"
+        autocomplete="off"
+        :model-value="modelValue"
+        @keydown.enter="modelValue = $event.target.value"
+        @blur="modelValue = $event.target.value"
+    />					
+</template>
