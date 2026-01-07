7ebe89737cbe66052723796278c081fd02b9e827
shahzeelahmed <ahmedshahzeel@gmail.com>
2026-01-08 02:31:41 +0530

    feat: add column highlighting feature in code editor

--- frontend/src2/components/Code.vue
+++ frontend/src2/components/Code.vue
@@ -22,7 +22,7 @@ import { javascript } from '@codemirror/lang-javascript'
 import { python } from '@codemirror/lang-python'
 import { MySQL, sql } from '@codemirror/lang-sql'
 import { syntaxTree } from '@codemirror/language'
-import { EditorView } from '@codemirror/view'
+import { Decoration, EditorView, ViewPlugin } from '@codemirror/view'
 import { onMounted, ref, watch } from 'vue'
 import { Codemirror } from 'vue-codemirror'
 import { tomorrow } from 'thememirror'
@@ -69,6 +69,10 @@ const props = defineProps({
 		type: Boolean,
 		default: true,
 	},
+	columnNames: {
+		type: Array,
+		default: () => [],
+	},
 })
 const emit = defineEmits(['inputChange', 'viewUpdate', 'focus', 'blur'])
 
@@ -108,10 +112,68 @@ const language =
 					tables: props.tables,
 		    })
 
+const columnHighlighter = ViewPlugin.fromClass(
+	class {
+		decorations
+
+		constructor(view) {
+			this.decorations = this.buildDecorations(view)
+		}
+
+		update(update) {
+			if (update.docChanged || update.viewportChanged) {
+				this.decorations = this.buildDecorations(update.view)
+			}
+		}
+
+		buildDecorations(view) {
+			if (!props.columnNames || props.columnNames.length === 0) {
+				return Decoration.none
+			}
+
+			const decorations = []
+			const columnSet = new Set(props.columnNames)
+			const doc = view.state.doc
+
+			for (let i = 1; i <= doc.lines; i++) {
+				const line = doc.line(i)
+				const text = line.text
+
+				// match only whole words ie `signups` but not `signups_today`
+				const wordRegex = /\b\w+\b/g
+				let match
+
+				while ((match = wordRegex.exec(text)) !== null) {
+					const word = match[0]
+					if (columnSet.has(word)) {
+						const from = line.from + match.index
+						const to = from + word.length
+						decorations.push(
+							Decoration.mark({
+								class: 'cm-column-highlight',
+							}).range(from, to)
+						)
+					}
+				}
+			}
+
+			return Decoration.set(decorations)
+		}
+	},
+	{
+		decorations: (v) => v.decorations,
+	}
+)
+
 const extensions = [language, closeBrackets(), tomorrow]
 if (props.multiLine) {
 	extensions.push(EditorView.lineWrapping)
 }
+
+if (props.columnNames && props.columnNames.length > 0) {
+	extensions.push(columnHighlighter)
+}
+
 const autocompletionOptions = {
 	activateOnTyping: true,
 	closeOnBlur: false,
