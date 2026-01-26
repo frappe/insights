def24a9e47a5c520f73218b3a00606667bec2712
Saqib Ansari <nextchamp.saqib@gmail.com>
2026-01-26 16:46:34 +0530

    fix: add freeze message
    (cherry picked from commit 292a2f7136521dd674880c536bf9cfad2e814533)

--- insights/insights/doctype/insights_data_source_v3/insights_data_source_v3.js
+++ insights/insights/doctype/insights_data_source_v3/insights_data_source_v3.js
@@ -4,8 +4,14 @@
 frappe.ui.form.on("Insights Data Source v3", {
 	refresh: function (frm) {
 		frm.add_custom_button("Test Connection", function () {
-			frm.call("test_connection", { raise_exception: true }).then((r) => {
-				frappe.msgprint("Connection Successful");
+			frm.call({
+				method: "test_connection",
+				doc: frm.doc,
+				freeze: true,
+				freeze_message: "Testing connection...",
+				callback: function (r) {
+					frappe.msgprint("Connection successful");
+				},
 			});
 		});
 	},
