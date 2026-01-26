08ea01830e509a7b386fb94fc3e41ad69fb20e1c
Saqib Ansari <nextchamp.saqib@gmail.com>
2026-01-26 16:56:23 +0530

    fix: remove unused status
    (cherry picked from commit fe17a857c3917f067b234ad10c59adb57f03cc0f)

--- insights/insights/doctype/insights_table_import_log/insights_table_import_log.py
+++ insights/insights/doctype/insights_table_import_log/insights_table_import_log.py
@@ -27,7 +27,7 @@ class InsightsTableImportLog(Document):
         row_size: DF.Float
         rows_imported: DF.Int
         started_at: DF.Datetime | None
-        status: DF.Literal["In Progress", "Completed", "Failed", "Queued"]
+        status: DF.Literal["In Progress", "Completed", "Failed"]
         table_name: DF.Data
         time_taken: DF.Int
     # end: auto-generated types
