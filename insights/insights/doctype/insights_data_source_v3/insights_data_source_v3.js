b65a4843879124895cc490ca81a29e2597ed4a33
Saqib Ansari <nextchamp.saqib@gmail.com>
2026-01-26 14:24:17 +0530

    feat: add "Test Connection" button
    (cherry picked from commit 6b60d51ee25ebac490ddcb6c34c1da359f824b17)

--- insights/insights/doctype/insights_data_source_v3/insights_data_source_v3.js
+++ insights/insights/doctype/insights_data_source_v3/insights_data_source_v3.js
@@ -2,5 +2,11 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Insights Data Source v3", {
-	refresh: function (frm) {},
+	refresh: function (frm) {
+		frm.add_custom_button("Test Connection", function () {
+			frm.call("test_connection", { raise_exception: true }).then((r) => {
+				frappe.msgprint("Connection Successful");
+			});
+		});
+	},
 });
