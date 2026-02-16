9615fb87b997bbaa08b17e81df775afd753932ec
shahzeelahmed <ahmedshahzeel@gmail.com>
2026-02-16 12:29:59 +0530

    feat(ui): separate filter editor dialog into setup and config

--- frontend/src2/dashboard/DashboardFilterEditor.vue
+++ frontend/src2/dashboard/DashboardFilterEditor.vue
@@ -1,16 +1,18 @@
 <script setup lang="ts">
 import { IconPicker } from 'frappe-ui/icons'
-import { computed, inject, nextTick, onMounted, reactive, ref } from 'vue'
+import { computed, inject, nextTick, reactive, ref, watch } from 'vue'
 import useChart from '../charts/chart'
 import { copy } from '../helpers'
 import { FIELDTYPES } from '../helpers/constants'
+import ColumnFilterValueSelector from '../query/components/ColumnFilterValueSelector.vue'
+import { getOperatorOptions, getValueSelectorType } from '../query/components/filter_utils'
+import NumberFilterPicker from '../query/components/NumberFilterPicker.vue'
+import RelativeDatePicker from '../query/components/RelativeDatePicker.vue'
 import { ColumnOption, FilterOperator } from '../types/query.types'
 import { WorkbookDashboardFilter } from '../types/workbook.types'
 import { Dashboard } from './dashboard'
 import { __ } from '../translation'
-import { Switch, Tabs } from 'frappe-ui'
-import { getOperatorOptions } from '@/utils'
-import { getValueSelectorType } from '../query/components/filter_utils'
+import { Switch, Tabs,DatePicker,DateRangePicker  } from 'frappe-ui'
 
 const dashboard = inject<Dashboard>('dashboard')!
 const props = defineProps<{ item: WorkbookDashboardFilter }>()
@@ -21,19 +23,13 @@ if (!filter.links) {
 }
 
 const tabIndex = ref(0)
-onMounted(() => {
-	requestAnimationFrame(() => {
-		nextTick(() => (tabIndex.value = 0))
-	})
-})
-
 const tabs = [
 	{
-		label: 'Setup',
+		label: __('Setup'),
 		value: 'setup',
 	},
 	{
-		label: 'Config',
+		label: __('Config'),
 		value: 'config',
 	},
 ]
@@ -170,54 +166,126 @@ function saveEdit() {
 		}"
 	>
 		<template #body-content>
-			<div class="flex flex-col gap-4">
-				<div class="flex gap-4">
-					<div class="flex flex-col gap-4 flex-1">
-						<FormControl
-							:label="__('Label')"
-							v-model="filter.filter_name"
-							:placeholder="__('Enter filter label...')"
-							autocomplete="off"
-						/>
-						<div class="flex flex-col gap-1.5">
-							<label class="block text-xs text-ink-gray-5"> Icon </label>
-							<IconPicker v-model="filter.icon" />
+			<div class="flex flex-col min-h-[20rem] max-h-[20rem]">
+				<Tabs  v-model="tabIndex" :tabs="tabs" class="-mt-6">
+					<template #tab-panel="{ tab }">
+						<div v-if="tab.value === 'setup'" class="flex flex-col gap-4 pt-2">
+							<div class="flex items-end gap-1.5 p-1">
+								<FormControl
+									class="flex-1"
+									:label="__('Label')"
+									v-model="filter.filter_name"
+									:placeholder="__('Enter filter label...')"
+									autocomplete="off"
+								/>
+								<FormControl
+									class="flex-1 flex-shrink-0"
+									v-model="filter.filter_type"
+									:label="__('Type')"
+									type="select"
+									:options="Object.keys(FILTER_TYPES)"
+									@update:modelValue="onFilterTypeChange"
+								/>
+							</div>
+							<div class="flex flex-col gap-1 border-t pt-3">
+								<label class="text-xs text-ink-gray-5">{{ __('Linked Charts') }}</label>
+								<div
+									v-for="link in linkOptions"
+									:key="link.name"
+									class="flex h-8 w-full items-center gap-2"
+								>
+									<Switch
+										size="sm"
+										:modelValue="enabledLinks.includes(link.name)"
+										@update:modelValue="toggleLink(link.name)"
+									></Switch>
+									<p class="flex-1 truncate text-base">{{ link.title }}</p>
+									<div v-if="enabledLinks.includes(link.name)" class="ml-auto flex-shrink-0">
+										<Autocomplete
+											class="min-w-[10rem]"
+											:placeholder="__('Select a column')"
+											:options="link.columns"
+											:modelValue="filter.links[link.name]"
+											@update:modelValue="filter.links[link.name] = $event.value"
+										/>
+									</div>
+								</div>
+							</div>
 						</div>
-					</div>
-					<FormControl
-						class="flex-1 flex-shrink-0"
-						v-model="filter.filter_type"
-						:label="__('Type')"
-						type="select"
-						:options="Object.keys(FILTER_TYPES)"
-						@update:modelValue="onFilterTypeChange"
-					/>
-				</div>
-				<div class="flex flex-col">
-					<div class="mb-1 text-p-sm text-gray-700">Linked Charts</div>
-					<div
-						v-for="link in linkOptions"
-						:key="link.name"
-						class="flex h-8 w-full items-center gap-2"
-					>
-						<Switch
-							size="sm"
-							:modelValue="enabledLinks.includes(link.name)"
-							@update:modelValue="toggleLink(link.name)"
-						></Switch>
-						<p class="flex-1 truncate text-base">{{ link.title }}</p>
-						<div v-if="enabledLinks.includes(link.name)" class="ml-auto flex-shrink-0">
-							<Autocomplete
-								class="min-w-[10rem]"
-								:placeholder="__('Select a column')"
-								:options="link.columns"
-								:modelValue="filter.links[link.name]"
-								@update:modelValue="filter.links[link.name] = $event.value"
-							/>
+						<div v-else-if="tab.value === 'config'" class="flex flex-col gap-4 pt-2">
+							<div class="flex flex-col gap-1.5 p-1">
+								<label class="block text-xs text-ink-gray-5">{{ __('Filter Icon') }}</label>
+								<IconPicker v-model="filter.icon" />
+							</div>
+							<div v-if="filter.filter_type" class="flex flex-col gap-2.5 border-t pt-3 p-1">
+								<div class="flex items-center justify-between">
+									<label class="text-xs text-ink-gray-5">{{ __('Default Value') }}</label>
+									<button
+										v-if="filter.default_operator"
+										class="text-xs text-ink-gray-5 hover:text-ink-gray-7"
+										@click="clearDefault"
+									>
+										{{ __('Clear') }}
+									</button>
+								</div>
+								<NumberFilterPicker
+									v-if="filter.filter_type === 'Number'"
+									v-model:operator="filter.default_operator"
+									v-model:value="(filter.default_value as number)"
+								/>
+								<template v-else>
+									<div class=" flex flex-row items-start gap-2">
+									<FormControl
+										type="select"
+										:placeholder="__('Select operator...')"
+										:modelValue="filter.default_operator"
+										:options="defaultOperatorOptions"
+										class="w-1/3"
+										@update:modelValue="onDefaultOperatorChange($event)"
+									/>
+									<template v-if="defaultValueSelectorType">
+										<DatePicker
+											v-if="defaultValueSelectorType === 'date'"
+											:range="false"
+											:modelValue="[filter.default_value as string]"
+											@update:modelValue="filter.default_value = $event[0]"
+										/>
+										<DateRangePicker
+											v-else-if="defaultValueSelectorType === 'date_range'"
+											:range="true"
+											v-model="(filter.default_value as string[])"
+										/>
+										<RelativeDatePicker
+											v-else-if="defaultValueSelectorType === 'relative_date'"
+											v-model="(filter.default_value as string)"
+										/>
+										<ColumnFilterValueSelector
+											v-else-if="defaultValueSelectorType === 'select'"
+											v-model="(filter.default_value as string[])"
+											:valuesProvider="defaultValuesProvider"
+										/>
+										<FormControl
+											v-else-if="defaultValueSelectorType === 'text'"
+											v-model="filter.default_value"
+											:placeholder="__('Value')"
+											autocomplete="off"
+										/>
+									</template>
+								</div>
+
+								</template>
+							</div>
 						</div>
-					</div>
-				</div>
+					</template>
+				</Tabs>
 			</div>
 		</template>
 	</Dialog>
 </template>
+
+<style scoped>
+:deep([role='tablist']) {
+	padding-bottom: 0;
+	gap: 1rem;
+}
+</style>
