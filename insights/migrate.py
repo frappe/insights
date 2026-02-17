e3d25b4ab89d2d8766a03362f066dcc0760d3d7d
shahzeelahmed <ahmedshahzeel@gmail.com>
2026-02-17 16:59:49 +0530

    fix: enable perms by default

--- insights/migrate.py
+++ insights/migrate.py
@@ -1,9 +1,16 @@
 # Copyright (c) 2022, Frappe Technologies Pvt. Ltd. and Contributors
 # See license.txt
 
-
-# import frappe
+import frappe
 
 
 def after_migrate():
-    pass
+    enable_permissions_by_default()
+
+
+def enable_permissions_by_default():
+    settings = frappe.get_single("Insights Settings")
+    if not settings.enable_permissions:
+        settings.enable_permissions = 1
+        settings.save()
+        frappe.db.commit()
