264744651e4879562ce5c52bd4cc8aa79b678017
shahzeelahmed <ahmedshahzeel@gmail.com>
2026-02-24 05:45:23 +0530

    fix: store favorite dashboards separately

--- frontend/src2/dashboard/dashboards.ts
+++ frontend/src2/dashboard/dashboards.ts
@@ -1,4 +1,4 @@
-import { useTimeAgo } from '@vueuse/core'
+import { get, useTimeAgo } from '@vueuse/core'
 import { call } from 'frappe-ui'
 import { reactive, ref } from 'vue'
 import { createInfoToast, createSuccessToast } from '../helpers/toasts'
@@ -19,21 +19,27 @@ export type DashboardListItem = {
 }
 
 const dashboards = ref<DashboardListItem[]>([])
+const favorites = ref<DashboardListItem[]>([])
 
 const loading = ref(false)
+const mapTimeAgo = (dashboard: any) => ({
+	...dashboard,
+	created_from_now: useTimeAgo(dashboard.creation),
+	modified_from_now: useTimeAgo(dashboard.modified),
+})
 async function fetchDashboards(search_term?: string, limit: number = 50) {
 	loading.value = true
-	dashboards.value = await call('insights.api.dashboards.get_dashboards', {
-		search_term,
-		limit,
-	})
-	dashboards.value = dashboards.value.map((dashboard: any) => ({
-		...dashboard,
-		created_from_now: useTimeAgo(dashboard.creation),
-		modified_from_now: useTimeAgo(dashboard.modified),
-	}))
+
+	const [regular, fav] = await Promise.all([
+		call('insights.api.dashboards.get_dashboards', { search_term, limit }),
+		call('insights.api.dashboards.get_dashboards',{get_favorites: true}),
+	])
+	console.log('dashboards', regular)
+	console.log('dashboards', fav)
+
+	dashboards.value = regular.map(mapTimeAgo)
+	favorites.value = fav.map(mapTimeAgo)
 	loading.value = false
-	return dashboards.value
 }
 
 const updatingPreviewImage = ref<Record<string, boolean>>({})
@@ -70,6 +76,7 @@ export default function useDashboardStore() {
 
 	return reactive({
 		dashboards,
+		favorites,
 		loading,
 		fetchDashboards,
 
