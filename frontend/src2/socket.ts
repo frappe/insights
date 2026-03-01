afd874c56f1d3e5865d84ac0a02b28792746abde
shahzeelahmed <ahmedshahzeel@gmail.com>
2026-03-02 02:32:15 +0530

    fix: get `socketio_port` from site config

--- frontend/src2/socket.ts
+++ frontend/src2/socket.ts
@@ -1,16 +1,23 @@
 import { io, Socket } from 'socket.io-client'
+import { socketio_port } from '../../../../sites/common_site_config.json'
 
 let socket: Socket
+
+declare global {
+	interface Window {
+		site_name: string
+	}
+}
 
 export function getSocket() {
 	if (socket) {
 		return socket
 	}
 
-	let socketio_port = window.socketio_port || 9000
+	let socket_port = socketio_port || 9000
 	let host = window.location.hostname
 	let siteName = import.meta.env.DEV ? host : window.site_name
-	let port = window.location.port ? `:${socketio_port}` : ''
+	let port = window.location.port ? `:${socket_port}` : ''
 	let protocol = port ? 'http' : 'https'
 	let url = `${protocol}://${host}${port}/${siteName}`
 
