8aab00f596e6e556b6842b28b85b555e417efb20
Saqib Ansari <nextchamp.saqib@gmail.com>
2026-03-16 15:47:22 +0530

    fix: dashboard preview generation (#942)
    * telemetry plugin calls `frappe.utils.telemetry.client.is_enabled` which isn't allowed for guest, so it throws to login page
    
    (cherry picked from commit 61a37f95ef08ef449c73e3b08043b082450e18cd)
    
    # Conflicts:
    #	frontend/src2/main.ts

--- frontend/src2/main.ts
+++ frontend/src2/main.ts
@@ -1,13 +1,20 @@
 import { frappeRequest, setConfig } from 'frappe-ui'
+import { spritePlugin } from 'frappe-ui/icons'
 import { GridItem, GridLayout } from 'grid-layout-plus'
 import { createPinia } from 'pinia'
-import { createApp } from 'vue'
+import { createApp, watchEffect } from 'vue'
 import App from './App.vue'
 import { registerControllers, registerGlobalComponents } from './globals.ts'
 import './index.css'
 import router from './router.ts'
 import { translationPlugin } from './translation.ts'
+<<<<<<< HEAD
 import { spritePlugin } from 'frappe-ui/icons'
+=======
+//@ts-ignore
+import { telemetryPlugin } from 'frappe-ui/frappe'
+import session from './session.ts'
+>>>>>>> 61a37f95 (fix: dashboard preview generation (#942))
 
 setConfig('resourceFetcher', frappeRequest)
 
@@ -19,6 +26,13 @@ app.use(router)
 app.use(spritePlugin)
 app.component('grid-layout', GridLayout)
 app.component('grid-item', GridItem)
+
+const stop = watchEffect(() => {
+	if (session.isLoggedIn) {
+		app.use(telemetryPlugin, { app_name: 'insights' })
+		stop()
+	}
+})
 
 app.config.errorHandler = (err, vm, info) => {
 	console.groupCollapsed('Unhandled Error in: ', info)
